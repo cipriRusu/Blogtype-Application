acaeaa5bed0f65bfea8491199b0f45c5ee8e04a8
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-22 07:04:56 -0700

    1)Removed comments 2)Implemented basic landing page routing/landing page 3)Imported render_template and used it for rendering basic view

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,21 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
 app = Flask(__name__)
 
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
-
 @app.route('/')
-@app.route('/hello')
-@app.route('/something')
-def hello():
-    # Render the page
-    return "Hello Python!"
+def index():
+    return render_template("index.html",
+    message = "Demo text message sent by server side")
+
+@app.route('/test')
+def test():
+    return "Some other Page"
 
 if __name__ == '__main__':
-    # Run the app server on localhost:4449
     app.run('localhost', 4449)

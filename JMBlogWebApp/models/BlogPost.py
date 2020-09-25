05bd0d3b113d10d44e6629fa54f8762933160e27
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-25 14:37:01 +0300

    Applied pylint exceptions

--- JMBlogWebApp/models/BlogPost.py
+++ JMBlogWebApp/models/BlogPost.py
@@ -1,3 +1,5 @@
+# pylint: disable=locally-disabled, multiple-statements, missing-module-docstring, missing-class-docstring, too-few-public-methods
+
 class BlogPost:
     def __init__(self, title, author, content):
         self.title = title

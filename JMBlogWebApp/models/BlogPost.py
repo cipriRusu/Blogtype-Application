7439e5512b3e41857ff3445013f4d5b26b7ec51a
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-25 17:33:43 +0300

    Implemented unique index identifier (for selection for removal) added pylint exceptions

--- JMBlogWebApp/models/BlogPost.py
+++ JMBlogWebApp/models/BlogPost.py
@@ -1,7 +1,9 @@
-# pylint: disable=locally-disabled, multiple-statements, missing-module-docstring, missing-class-docstring, too-few-public-methods
+# pylint: disable=locally-disabled, multiple-statements, missing-module-docstring, missing-class-docstring
+import uuid
 
 class BlogPost:
-    def __init__(self, title, author, content):
-        self.title = title
+    def __init__(self, author, title, content):
+        self.id = uuid.uuid4()
         self.author = author
+        self.title = title
         self.content = content

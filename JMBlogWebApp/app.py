d0742bd45f7c03a22e4a1cfe677aa3cb1f0287bb
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-26 20:41:10 +0300

    decoupled main application from interaction with mock_db (implemented repository)

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, request, redirect
 from models.blog_post import BlogPost
-from mock_db import MockDatabase
+from repository.base_repository import BaseRepository
 
 app = Flask(__name__)
-mock_database = MockDatabase()
+repository = BaseRepository()
 
 @app.route('/', methods=["GET"])
 def index():
-    return render_template("index.html", database=mock_database)
+    return render_template("index.html", database=repository.get_all())
 
 @app.route('/item/<uuid:current_index>')
 def content(current_index):
-    return render_template("Content.html", current=mock_database.get_by_index(current_index))
+    return render_template("Content.html", current=repository.get_by_id(current_index))
 
 @app.route('/addItem', methods=["GET", "POST"])
 def add_item():
     if request.method == "POST":
-        mock_database.add_post(
+        repository.add_post(
             BlogPost(request.form['NameInput'],
                      request.form['AuthorInput'],
                      request.form['ContentInput']))
@@ -25,18 +25,18 @@ def add_item():
 
 @app.route('/removeItem/<uuid:current_index>')
 def remove_item(current_index):
-    mock_database.remove(current_index)
+    repository.remove(current_index)
     return redirect('/')
 
 @app.route('/updateItem/<uuid:current_index>', methods=["GET", "POST"])
 def update_item(current_index):
     if request.method == "GET":
-        return render_template("UpdateItem.html", current=mock_database.get_by_index(current_index))
+        return render_template("UpdateItem.html", current=repository.get_by_id(current_index))
 
     if request.method == "POST":
-        mock_database.remove(current_index)
+        repository.remove(current_index)
 
-        mock_database.add_post(BlogPost(
+        repository.add_post(BlogPost(
             request.form['NameInput'],
             request.form['AuthorInput'],
             request.form['ContentInput']))

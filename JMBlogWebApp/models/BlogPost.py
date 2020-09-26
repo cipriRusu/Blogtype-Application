d9e009a637e56ae63b7279a1f9cae3715abcb8d8
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-26 10:46:24 +0300

    Implemented article preview in main index view

--- JMBlogWebApp/models/BlogPost.py
+++ JMBlogWebApp/models/BlogPost.py
@@ -1,9 +1,13 @@
 # pylint: disable=locally-disabled, multiple-statements, missing-module-docstring, missing-class-docstring
 import uuid
+import datetime
+from itertools import islice
 
 class BlogPost:
     def __init__(self, title, author, content):
         self.id = uuid.uuid4()
+        self.datetime = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
         self.author = author
         self.title = title
         self.content = content
+        self.preview = ' '.join(list(islice(content.split(), 0, 10)))

2c7be3ed18bd7489585960108db560723f0473ce
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-26 17:00:33 +0300

    removed deprecated method (replaced by iterator) marked private variable (standard)

--- JMBlogWebApp/mock_db.py
+++ JMBlogWebApp/mock_db.py
@@ -2,7 +2,7 @@ from models.blog_post import BlogPost
 
 class MockDatabase:
     def __init__(self):
-        self.db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy , \
+        self._db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy , \
         'text of the printing and typesetting industry. Lorem Ipsum has been the industry's , \
         standard dummy text ever since the 1500s, when an unknown printer took a galley of , \
         type and scrambled it to make a type specimen book. It has survived not only five centuries, , \
@@ -26,21 +26,18 @@ class MockDatabase:
         with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")]
 
     def __iter__(self):
-        for element in self.db:
+        for element in self._db:
             yield element
 
     def AddPost(self, blog_post):
-        self.db.append(blog_post)
+        self._db.append(blog_post)
     
     def GetByIndex(self, post_id):
-        for element in self.db:
+        for element in self._db:
             if element.post_id == post_id:
                 return element
 
-    def GetAll(self):
-        return self.db
-
     def Remove(self, current_index):
-        for element in self.db:
+        for element in self._db:
             if element.post_id == current_index:
-                self.db.remove(element)
+                self._db.remove(element)

d705c1bae6ead2ba945423a731721efddd877bf9
Cipri Rusu <cipri91rusu@gmail.com>
2020-09-27 10:37:03 +0300

    Included static resources into main app

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from views.post_manager import post_manager
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager)
 
 if __name__ == '__main__':

5b311e1d0a0118454d6982505cbb5dda8fc7d19f
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-01 15:54:04 +0300

    Implemented redirection on app start (landing page redirects to posts main page)

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect
 from views.post_manager import post_manager
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return redirect('/posts/allPosts', 302)
 
 if __name__ == '__main__':
     app.run('localhost', 4449)

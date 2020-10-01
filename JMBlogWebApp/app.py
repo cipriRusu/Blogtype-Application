d5ea73b72e5e2132ab0b59670c909a4fdd4a7230
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-01 18:42:14 +0300

    reintroduced variable (bugfix)

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -6,7 +6,7 @@ app.register_blueprint(post_manager)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch_all():
+def catch_all(path):
     return redirect('/posts/allPosts', 302)
 
 if __name__ == '__main__':

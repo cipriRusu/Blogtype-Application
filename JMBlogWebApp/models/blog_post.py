dce0e406e9c35b59cab76c81792d1c226d225574
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-03 23:48:57 +0300

    implemented extra constructor for blog_post model (for database arguments)

--- JMBlogWebApp/models/blog_post.py
+++ JMBlogWebApp/models/blog_post.py
@@ -10,3 +10,12 @@ class BlogPost:
         self.title = title
         self.content = content
         self.preview = ' '.join(list(islice(content.split(), 0, 10)))
+
+    def __init__(self, post_id, datetime, author, title, content):
+        self.post_id = post_id
+        self.datetime = datetime
+        self.author = author
+        self.title = title
+        self.content = content
+        self.preview = ' '.join(list(islice(content.split(), 0, 10)))
+

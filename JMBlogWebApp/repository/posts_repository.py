b7d2f4a63de9e20a3d9f34c9e7fde57f1e217398
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-04 16:08:09 +0300

    Implemented subclass hook

--- JMBlogWebApp/repository/posts_repository.py
+++ JMBlogWebApp/repository/posts_repository.py
@@ -1,6 +1,15 @@
 import abc
+from abc import ABCMeta
 
-class PostsRepository(abc.ABC):
+class PostsRepository(metaclass=ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, sub):
+        if cls is PostsRepository:
+            if any('__iter__' in Q.__dict__ for Q in sub.__mro__):
+                return True
+            return NotImplemented
+
     @abc.abstractmethod
     def add_post(self, item):
         pass

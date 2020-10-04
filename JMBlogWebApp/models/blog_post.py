b0429acfd6702ebd6c9fa4a3cc1622bb8efa02fa
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-04 13:27:20 +0300

    Removed unused constructor (adapted implementation for only 1 constructor via *args param)

--- JMBlogWebApp/models/blog_post.py
+++ JMBlogWebApp/models/blog_post.py
@@ -3,15 +3,7 @@ import datetime
 from itertools import islice
 
 class BlogPost:
-    def __init__(self, title, author, content):
-        self.post_id = uuid.uuid4()
-        self.datetime = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
-        self.author = author
-        self.title = title
-        self.content = content
-        self.preview = ' '.join(list(islice(content.split(), 0, 10)))
-
-    def __init__(self, post_id, datetime, author, title, content):
+    def __init__(self, post_id, datetime, title, author, content):
         self.post_id = post_id
         self.datetime = datetime
         self.author = author

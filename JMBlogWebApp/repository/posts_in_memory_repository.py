81100c3fe32e14641ec4cb2579faf9e037107adf
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-04 13:29:59 +0300

    Adapted mock data for current constructor

--- JMBlogWebApp/repository/posts_in_memory_repository.py
+++ JMBlogWebApp/repository/posts_in_memory_repository.py
@@ -1,9 +1,11 @@
 from models.blog_post import BlogPost
 from repository.posts_repository import PostsRepository
+import uuid
+import datetime
 
 class PostsInMemoryRepository(PostsRepository):
     def __init__(self):
-        self._db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy , \
+        self._db = [BlogPost(uuid.uuid4(), datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S"), "FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy , \
         'text of the printing and typesetting industry. Lorem Ipsum has been the industry's , \
         standard dummy text ever since the 1500s, when an unknown printer took a galley of , \
         type and scrambled it to make a type specimen book. It has survived not only five centuries, , \
@@ -11,7 +13,7 @@ class PostsInMemoryRepository(PostsRepository):
         1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop , \
         publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
 
-        BlogPost("SecondTitle", "SecondAuthor", "Lorem Ipsum is simply dummy text of the, \
+        BlogPost(uuid.uuid4(), datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S"),"SecondTitle", "SecondAuthor", "Lorem Ipsum is simply dummy text of the, \
         printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since, \
         the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen , \
         book. It has survived not only five centuries, but also the leap into electronic typesetting, , \
@@ -19,7 +21,7 @@ class PostsInMemoryRepository(PostsRepository):
         containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker , ~\
         including versions of Lorem Ipsum."),
 
-        BlogPost("ThirdTitle", "ThirdAuthor", "Lorem Ipsum is simply dummy text of the printing , \
+        BlogPost(uuid.uuid4(), datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S"),"ThirdTitle", "ThirdAuthor", "Lorem Ipsum is simply dummy text of the printing , \
         and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, , \
         when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived , \
         not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was, \

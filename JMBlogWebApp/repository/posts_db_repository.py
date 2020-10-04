5dd035a947fbac6211b6b040f623a0d4f7b194a9
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-04 13:29:41 +0300

    Implemented rest of CRUD operations

--- JMBlogWebApp/repository/posts_db_repository.py
+++ JMBlogWebApp/repository/posts_db_repository.py
@@ -7,7 +7,12 @@ class PostsDBRepository(PostsRepository):
         pass
 
     def add_post(self, item):
-        pass
+        conn = DBConnectionSetup()
+        current_connection = conn.get_connection('dbSetup/database.ini', 'postgresql_conn_data')
+        current_cursor = current_connection.cursor()
+        current_cursor.execute("INSERT INTO POSTS(post_id, datetime, author, title, content_field) VALUES(%s, %s, %s, %s, %s)", 
+                               (str(item.post_id), item.datetime, item.author, item.title, item.content))
+        current_connection.commit()
 
     def get_all(self):
         db = []
@@ -25,7 +30,18 @@ class PostsDBRepository(PostsRepository):
 
 
     def get_by_id(self, index):
-        pass
+        conn = DBConnectionSetup()
+        current_connection = conn.get_connection('dbSetup/database.ini', 'postgresql_conn_data')
+        current_cursor = current_connection.cursor()
+        current_cursor.execute("SELECT * FROM POSTS WHERE post_id=%s;", (str(index),))
+        current_element = current_cursor.fetchone()
+        conn.close_connection(current_connection, current_cursor)
+        return BlogPost(*current_element); 
 
     def remove(self, index):
+        conn = DBConnectionSetup()
+        current_connection = conn.get_connection('dbSetup/database.ini', 'postgresql_conn_data')
+        current_cursor = current_connection.cursor()
+        current_cursor.execute("DELETE FROM POSTS WHERE post_id=%s;", (str(index),))
+        current_connection.commit()
         pass

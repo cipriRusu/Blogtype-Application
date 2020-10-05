98873b2d7554a06c27f7cb98d1396ff6af5769c5
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-05 13:21:37 +0300

    Updated route redirection (simplified)

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -4,10 +4,9 @@ from views import post_manager
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager.post_manager)
 
-@app.route('/', defaults={'path': ''})
+@app.route('/')
 @app.route('/<path:path>')
-def catch_all(path):
-    #pylint: disable=unused-argument
+def catch_all():
     return redirect('/posts/', 302)
 
 if __name__ == '__main__':

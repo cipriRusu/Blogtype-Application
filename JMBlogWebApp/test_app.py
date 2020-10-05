9f3dff6dac105ba2154bc670200bb815430d26f4
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-05 09:49:00 +0300

    Added unit test for header content testing

--- JMBlogWebApp/test_app.py
+++ JMBlogWebApp/test_app.py
@@ -10,5 +10,11 @@ class TestApp(unittest.TestCase):
         response = tester.get('/posts/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    def test_app_main_header(self):
+        post_manager.dataSource = 0
+        tester = app.test_client(self)
+        response = tester.get('/posts/', content_type='html/text')
+        self.assertTrue(b'Blog App' in response.data)
+
 if __name__ == '__main__':
     unittest.main()

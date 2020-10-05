daf8bc0c35bb667e66fc066eeacc2ef431474d87
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-05 14:40:20 +0300

    removed unused module variable (TODO: change testing framework)

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -5,13 +5,11 @@ from views import post_manager
 
 class TestApp(unittest.TestCase):
     def test_app_response(self):
-        post_manager.dataSource = 0
         tester = app.test_client(self)
         response = tester.get('/posts/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
     def test_app_main_header(self):
-        post_manager.dataSource = 0
         tester = app.test_client(self)
         response = tester.get('/posts/', content_type='html/text')
         self.assertTrue(b'Blog App' in response.data)

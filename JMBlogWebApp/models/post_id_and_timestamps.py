abb83b230b5c6dcd2af8afa4d0297ce9e18b0da2
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 18:45:53 +0300

    shortened StampClass to 3 properties: Id (generate default id if not specified), creation_time(generate date/time at creation of object), edit_time(defaulted as null and modified as posts get edited)

--- JMBlogWebApp/models/post_id_and_timestamps.py
+++ JMBlogWebApp/models/post_id_and_timestamps.py
@@ -3,10 +3,9 @@ import datetime
 
 class PostIdAndTimestamps:
     def __init__(self):
-        _current_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
         self.post_id = uuid.uuid4()
-        self.creation_time = _current_time
-        self.edit_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
+        self.creation_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
+        self.edit_time = None
 
     def on_edit(self):
         self.edit_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")

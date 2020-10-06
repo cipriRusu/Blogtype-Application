62ce27259853a2cbe555ad3897b4e28c3d19661b
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 18:58:38 +0300

    Removed old unit testing dependencies (imported pytest) TODO: UnitTests

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -1,24 +1,13 @@
-import unittest
+import pytest
 from app import app
 from views import post_manager
+from repository.repository_factory import RepositoryFactory
+from repository.data_source_type import DataSourceType
 
-
-class TestApp(unittest.TestCase):
-    def test_app_response(self):
-        tester = app.test_client(self)
-        response = tester.get('/posts/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    def test_app_main_header(self):
-        tester = app.test_client(self)
-        response = tester.get('/posts/', content_type='html/text')
-        self.assertTrue(b'Blog App' in response.data)
-
-    def test_app_dynamic_content(self):
-        post_manager.dataSource = 0
-        tester = app.test_client(self)
-        response = tester.get('/posts/', content_type='html/text')
-        self.assertTrue(b'FirstTitle' in response.data)
+class TestPostManagerClass:
+    def test_first(self):
+        post_manager.test_mode = True
+        res = app.route('/posts/')
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()

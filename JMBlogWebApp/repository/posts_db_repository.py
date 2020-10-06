1b242eb12abd728954d214dec3f8f4475028d60f
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 18:47:18 +0300

    Implemented add_post with new parameter values (mapped to db) Implemented update post (mapped to database values and changing date/timestamp values accordingly)

--- JMBlogWebApp/repository/posts_db_repository.py
+++ JMBlogWebApp/repository/posts_db_repository.py
@@ -11,10 +11,16 @@ class PostsDBRepository(PostsRepository):
         current_connection = conn.get_connection('setup/database.ini', 'postgresql_conn_data')
         current_cursor = current_connection.cursor()
         current_cursor.execute("INSERT INTO POSTS \
-        (post_id, datetime, author, title, content_field) \
-        VALUES(%s, %s, %s, %s, %s)", (
-            str(item.post_id),
-            item.datetime,
+        (posts_id,\
+        creation_date,\
+        edit_date,\
+        author,\
+        title,\
+        post_content)\
+        VALUES(%s, %s, %s, %s, %s, %s)", (
+            str(item.stamp.post_id),
+            item.stamp.creation_time,
+            item.stamp.edit_time,
             item.author,
             item.title,
             item.content))
@@ -22,7 +28,24 @@ class PostsDBRepository(PostsRepository):
         conn.close_connection(current_connection, current_cursor)
 
     def update_post(self, item):
-        pass
+        conn = DBConnectionSetup()
+        current_connection = conn.get_connection('setup/database.ini', 'postgresql_conn_data')
+        current_cursor = current_connection.cursor()
+        current_cursor.execute("UPDATE POSTS SET\
+        creation_date = %s,\
+        edit_date = %s,\
+        author = %s,\
+        title = %s,\
+        post_content = %s \
+        WHERE posts_id =%s;",
+        (item.stamp.creation_time, 
+         item.stamp.edit_time,
+         item.author,
+         item.title,
+         item.content,
+         item.stamp.post_id))
+
+        conn.close_connection(current_connection, current_cursor)
 
     def get_all(self):
         all_elements = []
@@ -32,7 +55,11 @@ class PostsDBRepository(PostsRepository):
         current_cursor.execute('SELECT * FROM POSTS;')
 
         for item in current_cursor.fetchall():
-            element = BlogPost(*item)
+            element = BlogPost(item[3], item[4], item[5])
+            element.stamp.post_id = item[0]
+            element.stamp.creation_time = item[1]
+            element.stamp.edit_time = item[2]
+
             all_elements.append(element)
 
         conn.close_connection(current_connection, current_cursor)
@@ -43,15 +70,24 @@ class PostsDBRepository(PostsRepository):
         conn = DBConnectionSetup()
         current_connection = conn.get_connection('setup/database.ini', 'postgresql_conn_data')
         current_cursor = current_connection.cursor()
-        current_cursor.execute("SELECT * FROM POSTS WHERE post_id=%s;", (str(index),))
-        current_element = current_cursor.fetchone()
+        current_cursor.execute("SELECT * FROM POSTS WHERE posts_id=%s;", (str(index),))
+        item = current_cursor.fetchone()
         conn.close_connection(current_connection, current_cursor)
 
-        return BlogPost(*current_element)
+        element = BlogPost(
+            item[3], 
+            item[4], 
+            item[5])
+
+        element.stamp.post_id = item[0]
+        element.stamp.creation_time = item[1]
+        element.stamp.edit_time = item[2]
+
+        return element
 
     def remove(self, index):
         conn = DBConnectionSetup()
         current_connection = conn.get_connection('setup/database.ini', 'postgresql_conn_data')
         current_cursor = current_connection.cursor()
-        current_cursor.execute("DELETE FROM POSTS WHERE post_id=%s;", (str(index),))
+        current_cursor.execute("DELETE FROM POSTS WHERE posts_id=%s;", (str(index),))
         conn.close_connection(current_connection, current_cursor)

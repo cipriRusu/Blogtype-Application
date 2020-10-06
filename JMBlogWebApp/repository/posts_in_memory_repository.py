4c52e715916495650fe6f9a50cb3e4b1c2b8b4a3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 18:48:40 +0300

    harcoded UUID indices for elements (for testing purposes) converted index to string for comparison

--- JMBlogWebApp/repository/posts_in_memory_repository.py
+++ JMBlogWebApp/repository/posts_in_memory_repository.py
@@ -38,6 +38,15 @@ class PostsInMemoryRepository(PostsRepository):
                              publishing software like Aldus PageMaker including \
                              versions of Lorem Ipsum.")]
 
+        self._db[0].stamp.post_id = 'f9c3a576-28bc-4b63-931d-04d6488d2f0d'
+        self._db[0].stamp.creation_time = '2019-09-03 19:40:33'
+
+        self._db[1].stamp.post_id = 'daca57d1-c180-4e0a-8394-f5c95a5d5f23'
+        self._db[1].stamp.creation_time = '1991-03-02 12:30:44'
+
+        self._db[2].stamp.post_id = 'a656f973-5b82-462d-aff7-8d2c6c3e4fa2'
+        self._db[2].stamp.creation_time = '2017-04-05 21:40:32'
+
     def __iter__(self):
         for element in self._db:
             yield element
@@ -51,7 +60,7 @@ class PostsInMemoryRepository(PostsRepository):
 
     def get_by_id(self, index):
         for element in self._db:
-            if element.stamp.post_id == index:
+            if element.stamp.post_id == str(index):
                 return element
         raise Exception("no element found!")
 

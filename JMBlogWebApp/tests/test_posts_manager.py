52adff0d06b21fc0b2ef8fb4349d414b7912c98a
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 19:01:13 +0300

    UnitTestClass pytlint fixes (Temporary)

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -1,13 +1,9 @@
 import pytest
-from app import app
 from views import post_manager
-from repository.repository_factory import RepositoryFactory
-from repository.data_source_type import DataSourceType
 
 class TestPostManagerClass:
     def test_first(self):
         post_manager.test_mode = True
-        res = app.route('/posts/')
 
 if __name__ == '__main__':
     pytest.main()

b6b1eb852d7d6a1102f1426d7b8dae2cd27998a3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-06 11:19:59 +0300

    Implemented Id/Timestamp in base class Implemented update method for updating posts on edit

--- JMBlogWebApp/models/blog_post.py
+++ JMBlogWebApp/models/blog_post.py
@@ -1,11 +1,18 @@
+from models.post_id_and_timestamps import PostIdAndTimestamps
+
 class BlogPost:
-    def __init__(self, post_id, datetime, title, author, content):
-        self.post_id = post_id
-        self.datetime = datetime
+    def __init__(self, title, author, content):
+        self.stamp = PostIdAndTimestamps()
         self.author = author
         self.title = title
         self.content = content
         self.preview = content[0:30]
-   
+
     def __eq__(self, other):
-        return self.post_id == other.post_id
+        return self.stamp.post_id == other.stamp.post_id
+
+    def update(self, title, author, content):
+        self.title = title
+        self.author = author
+        self.content = content
+        self.stamp.on_edit()

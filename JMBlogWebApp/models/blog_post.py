4e4cf43a548e07248e3d4762c9fa8e75e722de07
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-07 14:55:28 +0300

    moved UUID back to blogpost implemented preview update

--- JMBlogWebApp/models/blog_post.py
+++ JMBlogWebApp/models/blog_post.py
@@ -1,18 +1,21 @@
-from models.post_id_and_timestamps import PostIdAndTimestamps
+import uuid
+from models.timestamp import TimeStamp
 
 class BlogPost:
     def __init__(self, title, author, content):
-        self.stamp = PostIdAndTimestamps()
+        self.post_id = uuid.uuid4()
+        self.stamp = TimeStamp()
         self.author = author
         self.title = title
         self.content = content
         self.preview = content[0:30]
 
     def __eq__(self, other):
-        return self.stamp.post_id == other.stamp.post_id
+        return self.post_id == other.post_id
 
     def update(self, title, author, content):
         self.title = title
         self.author = author
         self.content = content
+        self.preview = content[0:30]
         self.stamp.on_edit()

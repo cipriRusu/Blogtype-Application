d7f387963314f7a476be926b3bd78b2ff2f2e7b8
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-07 10:24:57 +0300

    Implemented some basic unit tests

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -1,9 +1,18 @@
 import pytest
+from app import app
 from views import post_manager
 
 class TestPostManagerClass:
-    def test_first(self):
+    def test_post_manager_basic_response(self):
         post_manager.TEST_MODE = True
+        app.register_blueprint(post_manager.post_manager)
+        result = app.test_client()
+        response = result.get('/posts/')
+        assert response.status == '200 OK'
 
-if __name__ == '__main__':
-    pytest.main()
+    def test_posts_manager_static_content_response(self):
+        post_manager.TEST_MODE = True
+        app.register_blueprint(post_manager.post_manager)
+        result = app.test_client()
+        response = result.get('/posts/')
+        assert b"Blog App" in response.data

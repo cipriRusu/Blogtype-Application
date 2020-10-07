80634f1f86c45c3813cabd1a16a1fc3949f78905
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-07 23:38:38 +0300

    renamed in memory database (for clarity)

--- JMBlogWebApp/repository/in_memory_data.py
+++ JMBlogWebApp/repository/in_memory_data.py
@@ -1,7 +1,7 @@
 import uuid
 from models.blog_post import BlogPost
 
-db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy ,\
+in_memory_db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy ,\
                              'text of the printing and typesetting industry. Lorem Ipsum \
                              has been the industry's , standard dummy text ever since the \
                              1500s,when an unknown printer took a galley of , type and scrambled \
@@ -36,9 +36,9 @@ db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dummy ,\
                              publishing software like Aldus PageMaker including \
                              versions of Lorem Ipsum.")]
 
-db[0].post_id = uuid.UUID('f9c3a576-28bc-4b63-931d-04d6488d2f0d')
-db[0].stamp.creation_time = '2019-09-03 19:40:33'
-db[1].post_id = uuid.UUID('daca57d1-c180-4e0a-8394-f5c95a5d5f23')
-db[1].stamp.creation_time = '1991-03-02 12:30:44'
-db[2].post_id = uuid.UUID('a656f973-5b82-462d-aff7-8d2c6c3e4fa2')
-db[2].stamp.creation_time = '2017-04-05 21:40:32'
+in_memory_db[0].post_id = uuid.UUID('f9c3a576-28bc-4b63-931d-04d6488d2f0d')
+in_memory_db[0].stamp.creation_time = '2019-09-03 19:40:33'
+in_memory_db[1].post_id = uuid.UUID('daca57d1-c180-4e0a-8394-f5c95a5d5f23')
+in_memory_db[1].stamp.creation_time = '1991-03-02 12:30:44'
+in_memory_db[2].post_id = uuid.UUID('a656f973-5b82-462d-aff7-8d2c6c3e4fa2')
+in_memory_db[2].stamp.creation_time = '2017-04-05 21:40:32'
--- JMBlogWebApp/repository/repository_factory.py
+++ JMBlogWebApp/repository/repository_factory.py
@@ -1,13 +1,13 @@
 from repository.posts_in_memory_repository import PostsInMemoryRepository
 from repository.posts_db_repository import PostsDBRepository
-from repository.in_memory_data import db
+from repository.in_memory_data import in_memory_db
 
 class RepositoryFactory:
     IS_TEST = True
     @staticmethod
     def get_source():
         if RepositoryFactory.IS_TEST:
-            return PostsInMemoryRepository(db)
+            return PostsInMemoryRepository(in_memory_db)
         if not RepositoryFactory.IS_TEST:
             return PostsDBRepository()
         return Exception('Data source unknown')

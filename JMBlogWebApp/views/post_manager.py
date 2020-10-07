647e45528b1c68297d8cef97e038c18c1abc7a13
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-07 23:33:14 +0300

    removed unused modules from post_manager removed allocation of data type

--- JMBlogWebApp/views/post_manager.py
+++ JMBlogWebApp/views/post_manager.py
@@ -1,26 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models.blog_post import BlogPost
-from repository.repository_factory import RepositoryFactory
-from repository.data_source_type import DataSourceType
+from repository.repository_factory import RepositoryFactory as repository
 
 post_manager = Blueprint('post_manager', __name__, url_prefix='/posts', template_folder='templates')
 
-TEST_MODE = True
-
-if TEST_MODE:
-    data_source = DataSourceType.LocalSource
-else:
-    data_source = DataSourceType.DatabaseSource
-
-repository = RepositoryFactory(data_source).get_source()
-
 @post_manager.route('/', methods=["GET"])
 def index():
-    return render_template("list_posts.html", database=repository.get_all())
+    return render_template("list_posts.html",
+                           database=repository.get_source().get_all())
 
 @post_manager.route('/<uuid:current_index>')
 def content(current_index):
-    return render_template("view_post.html", current=repository.get_by_id(current_index))
+    return render_template("view_post.html",
+                           current=repository.get_source().get_by_id(current_index))
 
 @post_manager.route('/add', methods=["GET", "POST"])
 def add_item():
@@ -29,27 +21,29 @@ def add_item():
             request.form['NameInput'],
             request.form['AuthorInput'],
             request.form['ContentInput'])
-        repository.add_post(to_add)
+
+        repository.get_source().add_post(to_add)
         return redirect(url_for('.content', current_index=to_add.post_id))
     return render_template("add_post.html")
 
 @post_manager.route('/remove/<uuid:current_index>')
 def remove_item(current_index):
-    repository.remove(current_index)
+    repository.get_source().remove(current_index)
     return redirect('/')
 
 @post_manager.route('/update/<uuid:current_index>', methods=["GET", "POST"])
 def update_item(current_index):
     if request.method == "GET":
-        return render_template("update_post.html", current=repository.get_by_id(current_index))
+        return render_template("update_post.html",
+                               current=repository.get_source().get_by_id(current_index))
 
     if request.method == "POST":
-        current = repository.get_by_id(current_index)
+        current = repository.get_source().get_by_id(current_index)
         current.update(
             request.form['NameInput'],
             request.form['AuthorInput'],
             request.form['ContentInput'])
-        repository.update_post(current)
+        repository.get_source().update_post(current)
 
         return redirect(url_for('.content', current_index=current.post_id))
     return Exception("Request type cannot be handled")

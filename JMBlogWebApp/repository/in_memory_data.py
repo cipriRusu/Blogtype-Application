55e31fe755c2d5ff51a03bb95a86123d282b1daf
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-08 16:35:57 +0300

    added specific data to post (for unit testing purposes)

--- JMBlogWebApp/repository/in_memory_data.py
+++ JMBlogWebApp/repository/in_memory_data.py
@@ -10,7 +10,8 @@ in_memory_db = [BlogPost("FirstTitle", "FirstAuthor", "Lorem Ipsum is simply dum
                              remaining essentially unchanged. It was popularised, in the,\
                              1960s with the release of Letraset sheets containing Lorem Ipsum \
                              passages,and more recently with desktop , publishing software like \
-                             Aldus PageMaker including versions of Lorem Ipsum."),
+                             Aldus PageMaker including versions of Lorem Ipsum.\
+                             Specific content first post"),
 
                 BlogPost("SecondTitle", "SecondAuthor", "Lorem Ipsum is simply \
                              dummy text of the, printing and typesetting industry. \

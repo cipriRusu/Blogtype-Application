2df93aec19260d8a12603cef000edb0840c86c1d
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-08 12:49:00 +0300

    internalized connection initialization and querying inside class (exposing only cursor for returning data)

--- JMBlogWebApp/setup/db_connection_setup.py
+++ JMBlogWebApp/setup/db_connection_setup.py
@@ -7,8 +7,22 @@ CONFIG_HEADER = 'postgresql_conn_data'
 class DBConnectionSetup():
     def __init__(self):
         self.config_parser = ConfigParser()
+        self.connection = self.__get_connection()
+        self.cursor = self.connection.cursor()
 
-    def get_connection(self):
+    def execute_query(self, query, args=None):
+        if args is None:
+            self.cursor.execute(query)
+        else:
+            self.cursor.execute(query, args)
+        return self.cursor
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.commit()
+        self.connection.close()
+
+    def __get_connection(self):
         self.config_parser.read(CONFIG_PATH)
         config_params = self.config_parser.items(CONFIG_HEADER)
         db_conn_dict = {}
@@ -20,8 +34,3 @@ class DBConnectionSetup():
 
         conn = psycopg2.connect(**db_conn_dict)
         return conn
-
-    def close_connection(self, connection, cursor):
-        connection.commit()
-        cursor.close()
-        connection.close()

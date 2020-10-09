12320ede57a040ded91c89a54da571cb7ae1cde3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-09 18:06:50 +0300

    Implemented unit tests for add/remove on posts

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -21,3 +21,39 @@ def test_loading_cotains_static_value_post_page(current_app):
 def test_loading_contains_dynamic_value_post_page(current_app):
     response = current_app.get('/posts/f9c3a576-28bc-4b63-931d-04d6488d2f0d').data
     assert b'Specific content first post' in response
+
+def test_add_post_route_status_code(current_app):
+    data = {
+        'NameInput': 'NewName',
+        'AuthorInput': 'NewAuthor',
+        'ContentInput': 'NewContent'}
+    post_response = current_app.post('/posts/add', data=data).status_code
+    assert post_response == 302
+
+def test_add_post_route_check_content_after_post(current_app):
+    data = {
+        'NameInput': 'NewName',
+        'AuthorInput': 'NewAuthor',
+        'ContentInput': 'NewContent'}
+
+    post_response = current_app.post('/posts/add', data=data, follow_redirects=True).data
+
+    assert b'NewName' in post_response
+
+def test_add_post_route_check_content_in_listing_after(current_app):
+    data = {
+        'NameInput': 'NewName',
+        'AuthorInput': 'NewAuthor',
+        'ContentInput': 'NewContent'}
+
+    add_post = current_app.post('/posts/add', data=data, follow_redirects=True)
+    assert add_post.status_code == 200
+
+    after_add = current_app.get('/', follow_redirects=True).data
+
+    assert b'NewName' in after_add
+
+def test_remove_post_route_check_content_in_listing_after(current_app):
+    remove_post = current_app.get('/posts/remove/f9c3a576-28bc-4b63-931d-04d6488d2f0d'
+                                  , follow_redirects=True).data
+    assert b'FirstTitle' not in remove_post

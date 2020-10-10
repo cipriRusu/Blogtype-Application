e4aec69b27a9fc402db958b67182cddc58f7ada9
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-10 15:47:10 +0300

    implemented file path checking (check ini file presence in /setup/ folder) imported setup blueprint and registered it in main application implemented branching for redirection in case of absent initialization

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,13 +1,18 @@
+from os import path
 from flask import Flask, redirect
 from views import post_manager
+from views import connection_manager
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager.post_manager)
+app.register_blueprint(connection_manager.connection_manager)
 
 @app.route('/')
 @app.route('/<path:path>')
 def catch_all():
-    return redirect('/posts/', 302)
+    if path.exists('./setup/database.ini'):
+        return redirect('/posts/', 302)
+    return redirect('/dbsetup/', 302)
 
 if __name__ == '__main__':
     app.run('localhost', 4449)

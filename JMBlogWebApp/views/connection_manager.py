6d0d94500a1f72235f4d96a05fd8e11029dd06b4
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-10 20:53:56 +0300

    implemented external file config parsing

--- JMBlogWebApp/views/connection_manager.py
+++ JMBlogWebApp/views/connection_manager.py
@@ -1,4 +1,6 @@
+import configparser
 from flask import Blueprint, render_template, request, redirect, url_for
+from setup.db_setup import DbSetup
 
 connection_manager = Blueprint(
     'connection_manager',
@@ -10,3 +12,13 @@ connection_manager = Blueprint(
 def database_connector():
     if request.method == "GET":
         return render_template("conn_config_db.html")
+    if request.method == "POST":
+        config = configparser.ConfigParser()
+        config['postgresql_conn_data'] = { 'host' : request.form['HostInput'],
+                                           'dbname' : request.form['DatabaseInput'],
+                                           'port' : request.form['PortInput'],
+                                           'user' : request.form['UsernameInput'],
+                                           'password' : request.form['PasswordInput']}
+
+        with open('./setup/database.ini', 'w') as configfile:
+            config.write(configfile)

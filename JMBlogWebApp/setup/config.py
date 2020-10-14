081c95d61e6c33e750be53e7b29c101b0bd94679
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-14 08:05:44 +0300

    Implemented Config class (encapsulated working with config.ini file)

---
+++ JMBlogWebApp/setup/config.py
@@ -0,0 +1,25 @@
+from configparser import ConfigParser
+
+CONFIG_PATH = 'setup/config.ini'
+
+class Config():
+    def __init__(self):
+        self._config_parser = ConfigParser()
+
+    def from_file(self, header):
+        self._config_parser.read(CONFIG_PATH)
+        config_params = self._config_parser.items(header)
+        db_conn_dict = {}
+
+        for config_param in config_params:
+            key = config_param[0]
+            value = config_param[1]
+            db_conn_dict[key] = value
+
+        return db_conn_dict
+
+    def to_file(self, source, header):
+        self._config_parser[header] = source
+        config.optionxform = lowercase
+        with open('./setup/config.ini', 'w+') as configfile:
+            self._config_parser.write(configfile)

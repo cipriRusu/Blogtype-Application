bf553e374880cf9d75d78aefde44c49a59420b7d
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-14 10:01:55 +0300

    Implemented configuration path checking

--- JMBlogWebApp/setup/config.py
+++ JMBlogWebApp/setup/config.py
@@ -1,13 +1,15 @@
+from os import path
 from configparser import ConfigParser
 
 CONFIG_PATH = 'setup/config.ini'
 
 class Config():
     def __init__(self):
+        self._config_path = CONFIG_PATH
         self._config_parser = ConfigParser()
 
     def from_file(self, header):
-        self._config_parser.read(CONFIG_PATH)
+        self._config_parser.read(self._config_path)
         config_params = self._config_parser.items(header)
         db_conn_dict = {}
 
@@ -22,3 +24,8 @@ class Config():
         self._config_parser[header] = source
         with open('./setup/config.ini', 'w+') as configfile:
             self._config_parser.write(configfile)
+
+    def is_configured(self):
+        if path.exists(self._config_path):
+            return True
+        return False

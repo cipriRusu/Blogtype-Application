618062baaca7bc8ffb23aa26a494285a63e97084
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-18 18:09:34 +0300

    inserted service class for post management (removed factory)

--- JMBlogWebApp/views/post_manager.py
+++ JMBlogWebApp/views/post_manager.py
@@ -1,18 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models.blog_post import BlogPost
-from repository.repository_factory import RepositoryFactory as repository
+from services.services import Services as src
 
-post_manager = Blueprint('post_manager', __name__, url_prefix='/posts', template_folder='templates')
+post_manager = Blueprint('post_manager', __name__, url_prefix='/', template_folder='templates')
 
 @post_manager.route('/', methods=["GET"])
 def index():
-    return render_template("list_posts.html",
-                           database=repository.get_source().get_all())
+    return render_template("list_posts.html", database=src().get_service('test').get_all())
 
 @post_manager.route('/<uuid:current_index>')
 def content(current_index):
-    return render_template("view_post.html",
-                           current=repository.get_source().get_by_id(current_index))
+    return render_template("view_post.html", current=src().get_service('test').get_by_id(current_index))
 
 @post_manager.route('/add', methods=["GET", "POST"])
 def add_item():
@@ -22,28 +20,28 @@ def add_item():
             request.form['AuthorInput'],
             request.form['ContentInput'])
 
-        repository.get_source().add_post(to_add)
+        src().get_service('test').add_post(to_add)
         return redirect(url_for('.content', current_index=to_add.post_id))
     return render_template("add_post.html")
 
 @post_manager.route('/remove/<uuid:current_index>')
 def remove_item(current_index):
-    repository.get_source().remove(current_index)
+    src().get_service('test').remove(current_index)
     return redirect('/')
 
 @post_manager.route('/update/<uuid:current_index>', methods=["GET", "POST"])
 def update_item(current_index):
     if request.method == "GET":
         return render_template("update_post.html",
-                               current=repository.get_source().get_by_id(current_index))
+                               current=src().get_service('database').get_by_id(current_index))
 
     if request.method == "POST":
-        current = repository.get_source().get_by_id(current_index)
+        current = src().get_service().get_by_id(current_index)
         current.update(
             request.form['NameInput'],
             request.form['AuthorInput'],
             request.form['ContentInput'])
-        repository.get_source().update_post(current)
+        src().get_service('test').update_post(current)
 
         return redirect(url_for('.content', current_index=current.post_id))
     return Exception("Request type cannot be handled")

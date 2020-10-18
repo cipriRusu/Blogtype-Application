4de4535d11e6c97b7e94a8ff5b322eed569fba5c
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-18 18:11:34 +0300

    removed redundant redirection code

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,19 +1,11 @@
 from flask import Flask, redirect
 from views import post_manager
 from views import setup_manager
-from setup.config import Config
-from repository.repository_factory import RepositoryFactory as current_repository
+from services.services import Services
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager.post_manager)
 app.register_blueprint(setup_manager.setup_manager)
-
-@app.route('/')
-@app.route('/<path:path>')
-def catch_all():
-    if Config().is_configured():
-        return redirect('/posts/', 302)
-    return redirect('/setup/', 302)
 
 if __name__ == '__main__':
     app.run('localhost', 4449)

f696e510ba5c3e2512f009763cd7d215c4a35134
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-19 22:44:48 +0300

    reintroduced posts redirect from root inside main app

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -7,5 +7,10 @@ app = Flask(__name__, static_url_path="", static_folder="static")
 app.register_blueprint(post_manager.post_manager)
 app.register_blueprint(setup_manager.setup_manager)
 
+@app.route('/')
+@app.route('/<path:path>')
+def catch_all():
+    return redirect('/posts/', 302)
+
 if __name__ == '__main__':
     app.run('localhost', 4449)

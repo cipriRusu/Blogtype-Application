a8f161ba5f538e0e6d623b0e661b800f7a847b14
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-20 23:01:11 +0300

    inserted Service from app_context

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -4,13 +4,16 @@ from views import setup_manager
 from services.services import Services
 
 app = Flask(__name__, static_url_path="", static_folder="static")
-app.register_blueprint(post_manager.post_manager)
-app.register_blueprint(setup_manager.setup_manager)
 
-@app.route('/')
-@app.route('/<path:path>')
-def catch_all():
-    return redirect('/posts/', 302)
+with app.app_context():
+    app.config['DATA_SOURCE'] = Services().get_service('test')
+    app.register_blueprint(post_manager.post_manager)
+    app.register_blueprint(setup_manager.setup_manager)
+
+    @app.route('/')
+    @app.route('/<path:path>')
+    def catch_all():
+        return redirect('/posts/', 302)
 
 if __name__ == '__main__':
     app.run('localhost', 4449)

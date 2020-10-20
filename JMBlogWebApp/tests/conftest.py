51649c62c8c92679f2a633b2ae0142711a060b68
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-20 22:59:42 +0300

    added test setting in fixture (TODO, create separate fixture for configured/not configured/test app)

--- JMBlogWebApp/tests/conftest.py
+++ JMBlogWebApp/tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 from app import app
+from services.services import Services
 
 @pytest.fixture
 def current_app():
+    app.config['DATA_SOURCE'] = Services().get_service('test')
     response_from = app.test_client()
     return response_from

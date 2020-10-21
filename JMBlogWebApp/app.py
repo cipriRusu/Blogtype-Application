0fb542f9457247e64e8bdb573d1d852dd6314c44
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-21 11:59:03 +0300

    changed default parameter to database

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -6,7 +6,7 @@ from services.services import Services
 app = Flask(__name__, static_url_path="", static_folder="static")
 
 with app.app_context():
-    app.config['DATA_SOURCE'] = Services().get_service('test')
+    app.config['DATA_SOURCE'] = Services().get_service('database')
     app.register_blueprint(post_manager.post_manager)
     app.register_blueprint(setup_manager.setup_manager)
 

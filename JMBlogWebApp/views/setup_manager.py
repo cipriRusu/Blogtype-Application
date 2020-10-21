4b1daf6e54da9fbca358aa88da14fca47bf6a7f3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-21 23:53:30 +0300

    added config checking and creation in setup blueprint (TODO: move logic and checking in service class for implementing dependency injection)

--- JMBlogWebApp/views/setup_manager.py
+++ JMBlogWebApp/views/setup_manager.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from setup.db_setup import DbSetup
+from setup.db_connect  import DbConnect
 from setup.config import Config
 
 setup_manager = Blueprint(
@@ -13,12 +14,11 @@ def database_connector():
     if request.method == "GET":
         return render_template("setup.html")
     if request.method == "POST":
-        Config().to_file(request.form, 'db_connection')
-        DbSetup(
-            {'host': request.form['host'],
-             'user': request.form['user'],
-             'password': request.form['password'],
-             'database' : request.form['database']})
-
-        return redirect(url_for('post_manager.index'))
+        if not Config().is_configured():
+            Config().to_file(request.form, 'db_connection')
+            DbSetup(**Config().from_file('db_connection'))
+            DbConnect(True, **request.form)
+        if Config.is_configured():
+            DbConnect(True, **request.form)
+            return redirect(url_for('post_manager.index'))
     return Exception("Cannot handle current request")

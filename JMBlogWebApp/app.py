1e58eca307ed1a4aa8e718103860b4b655db13e9
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-21 23:53:52 +0300

    added redirection to setup page by default

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,19 +1,17 @@
 from flask import Flask, redirect
 from views import post_manager
 from views import setup_manager
-from services.services import Services
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
 with app.app_context():
-    app.config['DATA_SOURCE'] = Services().get_service('database')
     app.register_blueprint(post_manager.post_manager)
     app.register_blueprint(setup_manager.setup_manager)
 
     @app.route('/')
     @app.route('/<path:path>')
     def catch_all():
-        return redirect('/posts/', 302)
+        return redirect('/setup', 302)
 
 if __name__ == '__main__':
     app.run('localhost', 4449)

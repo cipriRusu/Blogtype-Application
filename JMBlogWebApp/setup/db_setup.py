901696c5a98c0da5d24b23069aad82a4fa3e9696
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-23 20:17:37 +0300

    added connection opening and closing in DbSetup

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -4,23 +4,29 @@ from setup.db_connect import DbConnect
 class DbSetup():
     def __init__(self, **connection_params):
         self._params = connection_params
-        self._connection = self.create_database()
 
     def create_database(self):
 
         conn = DbConnect(hasDb=False, **self._params)
+        conn.create_connection()
 
         if not conn.contains_database(self._params.get('database')):
 
             conn.execute(scripts.CREATE_DATABASE_SCRIPT % self._params['database'])
+            conn.close_connection()
 
             conn = DbConnect(**self._params)
+            conn.create_connection()
 
             conn.execute(scripts.CREATE_TABLE_SCRIPT)
+            conn.close_connection()
 
         conn = DbConnect(**self._params)
+        conn.create_connection()
 
         if not conn.contains_table():
             conn.execute(scripts.CREATE_TABLE_SCRIPT)
+
+        conn.close_connection()
 
         return conn

c34529e5dd614073c0dc6d805e7da30e6c8c39a3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-23 09:59:53 +0300

    added separation for data source (test/db)

--- JMBlogWebApp/tests/conftest.py
+++ JMBlogWebApp/tests/conftest.py
@@ -2,8 +2,10 @@ import pytest
 from app import app
 from services.services import Services
 
+Services.IS_TEST = True
+
 @pytest.fixture
 def current_app():
-    app.config['DATA_SOURCE'] = Services().get_service('test')
     response_from = app.test_client()
+    app.config['TEST'] = True
     return response_from

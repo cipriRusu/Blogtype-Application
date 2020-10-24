ab573475e4cd02a0f7c79abe9e6c709f4aeb4bda
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-24 22:25:39 +0300

    replaced hardcoded strings with constants

--- JMBlogWebApp/views/post_manager.py
+++ JMBlogWebApp/views/post_manager.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from setup import services_listing as services
 from models.blog_post import BlogPost
 from services.services import Services
 
@@ -7,12 +8,16 @@ post_manager = Blueprint('post_manager', __name__, url_prefix='/posts', template
 @post_manager.route('/', methods=["GET"])
 def index():
     return render_template("list_posts.html",
-                           database=Services().get_service('data_source').get_all())
+                           database=Services
+                           .get_service(services.DATA_SOURCE)
+                           .get_all())
 
 @post_manager.route('/<uuid:current_index>')
 def content(current_index):
     return render_template("view_post.html",
-                           current=Services().get_service('data_source').get_by_id(current_index))
+                           current=Services
+                           .get_service(services.DATA_SOURCE)
+                           .get_by_id(current_index))
 
 @post_manager.route('/add', methods=["GET", "POST"])
 def add_item():
@@ -22,30 +27,30 @@ def add_item():
             request.form['AuthorInput'],
             request.form['ContentInput'])
 
-        Services().get_service('data_source').add_post(to_add)
+        Services.get_service(services.DATA_SOURCE).add_post(to_add)
         return redirect(url_for('.content', current_index=to_add.post_id))
     return render_template("add_post.html")
 
 @post_manager.route('/remove/<uuid:current_index>')
 def remove_item(current_index):
-    Services().get_service('data_source').remove(current_index)
+    Services.get_service(services.DATA_SOURCE).remove(current_index)
     return redirect('/posts')
 
 @post_manager.route('/update/<uuid:current_index>', methods=["GET", "POST"])
 def update_item(current_index):
     if request.method == "GET":
         return render_template("update_post.html",
-                               current=Services()
-                               .get_service('data_source')
+                               current=Services
+                               .get_service(services.DATA_SOURCE)
                                .get_by_id(current_index))
 
     if request.method == "POST":
-        current = Services().get_service('data_source').get_by_id(current_index)
+        current = Services.get_service(services.DATA_SOURCE).get_by_id(current_index)
         current.update(
             request.form['NameInput'],
             request.form['AuthorInput'],
             request.form['ContentInput'])
-        Services().get_service('data_source').update_post(current)
+        Services.get_service(services.DATA_SOURCE).update_post(current)
 
         return redirect(url_for('.content', current_index=current.post_id))
     return Exception("Request type cannot be handled")

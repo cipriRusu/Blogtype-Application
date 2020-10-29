d9bd23c22a47eafa5b6e23310a058e055f0e1782
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-29 13:24:11 +0200

    added configuration parameter in DbSetup (reimplemented parameter inputting via ctor)

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,35 +1,29 @@
 import setup.postgres_scripts as scripts
 from setup.db_connect import DbConnect
-from setup.config import Config
 
 class DbSetup():
-    def __init__(self):
-        self._config = None
+    def __init__(self, db_config):
+        self._config = db_config
 
     def create_database(self):
-        self._config = Config().from_file('db_connection')
-        conn = DbConnect(hasDb=False)
-        conn.get_parameters(**self._config)
+        conn = DbConnect(self._config, hasDb=False)
         conn.create_connection()
 
-        if not conn.contains_database(self._config.get('database')):
+        if not conn.contains_database(self._config.from_file('db_connection').get('database')):
 
-            conn.execute(scripts.CREATE_DATABASE_SCRIPT % self._config['database'])
+            conn.execute(scripts.CREATE_DATABASE_SCRIPT % self._config.from_file['database'])
             conn.close_connection()
 
-            conn = DbConnect()
-            conn.get_parameters(**self._config)
+            conn = DbConnect(self._config)
             conn.create_connection()
 
             conn.execute(scripts.CREATE_TABLE_SCRIPT)
             conn.close_connection()
 
-        conn = DbConnect()
-        conn.get_parameters(**self._config)
+        conn = DbConnect(self._config)
         conn.create_connection()
 
         if not conn.contains_table():
             conn.execute(scripts.CREATE_TABLE_SCRIPT)
 
         conn.close_connection()
-

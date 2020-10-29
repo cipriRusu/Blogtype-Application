d3686581453d27dc531d0f8c03dfffd49fa0e5a5
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-29 13:21:16 +0200

    reimplemented Services() class (services parameterizing via constructors)

--- JMBlogWebApp/services/services.py
+++ JMBlogWebApp/services/services.py
@@ -6,30 +6,22 @@ from repository.posts_db_repository import PostsDBRepository as db
 from repository.posts_in_memory_repository import PostsInMemoryRepository as test_db
 from repository.in_memory_data import in_memory_db as test_data
 
-class Services:
+class Services():
     IS_TEST = False
     def __init__(self):
         pass
 
-    services_container = {
-        service.DATA_SOURCE: db(),
-        service.CONFIGURE: config(),
-        service.CONNECT: connect(),
-        service.SETUP: setup()}
+    test = {service.DATA_SOURCE: test_db(test_data),
+            service.CONFIGURE: config(),
+            service.CONNECT: connect(config()),
+            service.SETUP: setup(config())}
 
-    test_container = {
-        service.DATA_SOURCE: test_db(test_data),
-        service.CONFIGURE: config(),
-        service.CONNECT: connect(),
-        service.SETUP: setup()}
 
-    singletons = {}
+    production = {service.DATA_SOURCE: db(connect(config())),
+               service.CONFIGURE: config(),
+               service.CONNECT: connect(config()),
+               service.SETUP: setup(config())}
 
     @classmethod
     def get_service(cls, service_name):
-        if service_name not in Services.singletons:
-            Services.singletons[service_name] = (Services.test_container[service_name]
-                                                 if Services.IS_TEST
-                                                 else Services.services_container[service_name])
-            return Services.singletons[service_name]
-        return Services.singletons[service_name]
+        return Services.test[service_name] if Services.IS_TEST else Services.production[service_name]

dd83fe5b1e799b45b337cba18c797f1cedc7ba5a
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-31 17:29:07 +0200

    Added unit tests for setup module (redirect if configured)

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -90,3 +90,11 @@ def test_edit_post_route_check_content_in_post_after_edit(configured_app):
 
     assert b'SecondTitle' not in edit_post
     assert b'NewName' in edit_post
+
+def test_setup_manager_redirect_if_valid_configuration(configured_app):
+    returned_page = configured_app.get('/setup/', follow_redirects=True).data
+    assert b"Blog App" in returned_page
+
+def test_setup_manager_does_not_redirect_if_unconfigured(unconfigured_app):
+    returned_page = unconfigured_app.get('/setup/', follow_redirects=True).data
+    assert b"Connection Setup:" in returned_page

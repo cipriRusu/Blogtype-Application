e55310041c9aa8e05bfaafd6bfa36af20ca3fcc3
Cipri Rusu <cipri91rusu@gmail.com>
2020-10-31 14:47:33 +0200

    Simplified implementation:
    Working only with _connection (no db verification, throwing exception in case connection failure)
    
    Removed other unused redundant code

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,29 +1,18 @@
 import setup.postgres_scripts as scripts
-from setup.db_connect import DbConnect
 
 class DbSetup():
-    def __init__(self, db_config):
-        self._config = db_config
+    def __init__(self, db_connection):
+        self._connection = db_connection
 
     def create_database(self):
-        conn = DbConnect(self._config, hasDb=False)
-        conn.create_connection()
+        try:
+            self._connection.create_connection()
+        except:
+            raise Exception(self._connection,
+                            "Connection cannot be established\
+                             as there is no local available database")
 
-        if not conn.contains_database(self._config.from_file('db_connection').get('database')):
+        if not self._connection.contains_table():
+            self._connection.execute(scripts.CREATE_TABLE_SCRIPT)
 
-            conn.execute(scripts.CREATE_DATABASE_SCRIPT % self._config.from_file['database'])
-            conn.close_connection()
-
-            conn = DbConnect(self._config)
-            conn.create_connection()
-
-            conn.execute(scripts.CREATE_TABLE_SCRIPT)
-            conn.close_connection()
-
-        conn = DbConnect(self._config)
-        conn.create_connection()
-
-        if not conn.contains_table():
-            conn.execute(scripts.CREATE_TABLE_SCRIPT)
-
-        conn.close_connection()
+        self._connection.close_connection()

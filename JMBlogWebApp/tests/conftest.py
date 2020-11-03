cfd65aa0579199305ee3a014b14a5d8a1824f3ba
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-04 00:29:23 +0200

    added patching on database configuration instead of parent

--- JMBlogWebApp/tests/conftest.py
+++ JMBlogWebApp/tests/conftest.py
@@ -7,14 +7,14 @@ Services.IS_TEST = True
 
 @pytest.fixture
 def configured_app():
-    with patch('setup.config.Config.is_configured', return_value=True):
+    with patch('setup.db_config.DbConfig.is_configured', return_value=True):
         response_from = app.test_client()
         app.config['TEST'] = True
         yield response_from
 
 @pytest.fixture
 def unconfigured_app():
-    with patch('setup.config.Config.is_configured', return_value=False):
+    with patch('setup.db_config.DbConfig.is_configured', return_value=False):
         response_from = app.test_client()
         app.config['TEST'] = True
         yield response_from

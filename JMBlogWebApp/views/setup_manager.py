d9285cc563a51c305982f00269b68c94b9162061
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-03 12:36:55 +0200

    injected arguments into method through @inject decorator and removed unnecessary imports from module

--- JMBlogWebApp/views/setup_manager.py
+++ JMBlogWebApp/views/setup_manager.py
@@ -1,8 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from setup import services_listing as services
-from setup.config import Config
-from setup.db_setup import DbSetup
-from services.services import Services
 from decorators import decorators
 
 setup_manager = Blueprint(
@@ -13,7 +10,7 @@ setup_manager = Blueprint(
 
 @setup_manager.route('/', methods=["GET", "POST"])
 @decorators.inject
-def database_connector(current_config: "Config", current_setup: "DbSetup"):
+def database_connector(current_config: services.CONFIGURE, current_setup: services.SETUP):
     if request.method == "GET":
         if current_config.is_configured():
             return redirect(url_for('post_manager.index'))

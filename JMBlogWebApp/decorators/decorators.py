88c349b66340f71b0d3c4272848170b84c8360e8
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-03 12:35:55 +0200

    added injection decorator (returning named arguments through _annotations_ property of function) and returning appropriate services as needed

--- JMBlogWebApp/decorators/decorators.py
+++ JMBlogWebApp/decorators/decorators.py
@@ -12,3 +12,11 @@ def config_check(function):
             return function(*args, **kwargs)
         return redirect(url_for('setup_manager.database_connector'))
     return wrapper
+
+def inject(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        for element in func.__annotations__.items():
+            kwargs[element[0]] = Services.get_service(element[1])
+        return func(*args, **kwargs)
+    return wrapper

0c7fc67ad1332c2a663a3e88cf05dd13fd32103b
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-05 11:32:27 +0200

    saving / loading configuration as model class (sending and receiving model as parameter)

--- JMBlogWebApp/setup/db_config.py
+++ JMBlogWebApp/setup/db_config.py
@@ -5,11 +5,10 @@ class DbConfig(Config):
     DB_CONNECTION_HEADER = 'db_connection'
 
     def load_configuration(self):
-        return super().from_file(DbConfig.DB_CONNECTION_HEADER)
+        return DatabaseConfiguration(**super().from_file(DbConfig.DB_CONNECTION_HEADER))
 
     def save_configuration(self, received_stats):
-        super().to_file(DatabaseConfiguration(**received_stats).__dict__,
-                        DbConfig.DB_CONNECTION_HEADER)
+        super().to_file(received_stats, DbConfig.DB_CONNECTION_HEADER)
 
     def is_configured(self):
         return super().is_configured() and super().has_section(DbConfig.DB_CONNECTION_HEADER)

ec2b40918c6a940dcfeb96d59c2e4ee1019262f9
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-05 11:35:17 +0200

    Sending form data through model class (instead of direct dictionary)

--- JMBlogWebApp/views/setup_manager.py
+++ JMBlogWebApp/views/setup_manager.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from setup import services_listing as services
 from decorators import decorators
+from flask import Blueprint, render_template, request, redirect, url_for
+from models.database_configuration import DatabaseConfiguration
+from setup import services_listing as services
 
 setup_manager = Blueprint(
     'setup_manager',
@@ -17,7 +18,7 @@ def database_connector(current_db_config: services.DB_CONFIGURATION, setup: serv
         return render_template("setup.html")
 
     if request.method == "POST":
-        current_db_config.save_configuration(request.form)
+        current_db_config.save_configuration(DatabaseConfiguration(**request.form))
         setup.create_database()
         return redirect(url_for('post_manager.index'))
     return Exception("Cannot handle current request")

0c7c088391c4d6ee9b3dde4292b30e489c45c9c4
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-05 11:33:32 +0200

    loading configuration through model method (instead of obtaining it directly)

--- JMBlogWebApp/setup/db_connect.py
+++ JMBlogWebApp/setup/db_connect.py
@@ -4,11 +4,13 @@ import setup.postgres_scripts as scripts
 class DbConnect():
     def __init__(self, config):
         self._config = config
+        self._loaded_config = None
         self._connection = None
         self._cursor = None
 
     def create_connection(self):
-        self._connection = psycopg2.connect(**self._config.load_configuration())
+        self._loaded_config = self._config.load_configuration()
+        self._connection = psycopg2.connect(**self._loaded_config.get_dictionary())
         self._connection.autocommit = True
         self._cursor = self._connection.cursor()
 

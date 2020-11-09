0d6b89f3d66eb0d3d1307288eaeec811ff025b40
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-09 14:00:59 +0200

    renamed User model class parameters

--- JMBlogWebApp/models/user.py
+++ JMBlogWebApp/models/user.py
@@ -1,9 +1,8 @@
 class User:
-    def __init__():
-        pass
-    self.user_id = id
-    self.user_name = name
-    self.user_email = email
-    self.user_password = password
-    self.user_created_at = created_at
-    self.user_modifed_at = modified_at
+    def __init__(self, user_id, user_name, user_email, user_password, user_created_at, user_modified_at):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_email = user_email
+        self.user_password = user_password
+        self.user_created_at = user_created_at
+        self.user_modifed_at = user_modified_at

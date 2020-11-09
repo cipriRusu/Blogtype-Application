88656f0f5834870f3ff6cca974b856b7010a5bc7
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-09 14:00:01 +0200

    implemented and imported services for test/production users

--- JMBlogWebApp/services/services.py
+++ JMBlogWebApp/services/services.py
@@ -3,27 +3,34 @@ from setup.db_config import DbConfig
 from setup.db_connect  import DbConnect
 from setup.db_setup import DbSetup
 from repository.posts_db_repository import PostsDBRepository
+from repository.users_db_repository import UsersDBRepository
 from repository.posts_in_memory_repository import PostsInMemoryRepository
-from repository.in_memory_data import in_memory_db as test_data
+from repository.users_in_memory_repository import UsersInMemoryRepository
+from repository.in_memory_data import in_memory_posts as test_data_posts
+from repository.in_memory_data import in_memory_users as test_data_users
 
 class Services():
-    IS_TEST = False
+    IS_TEST = True
     def __init__(self):
         pass
 
     db_configuration = DbConfig()
     connection = DbConnect(db_configuration)
     setup = DbSetup(connection)
+    test_users_repository = UsersInMemoryRepository(test_data_users)
+    test_posts_repository = PostsInMemoryRepository(test_data_posts)
     posts_repository = PostsDBRepository(connection)
-    test_repository = PostsInMemoryRepository(test_data)
+    users_repository = UsersDBRepository(connection)
 
     test = {service.DB_CONFIGURATION: db_configuration,
-            service.DATA_SOURCE: test_repository,
+            service.DATA_SOURCE_POSTS: test_posts_repository,
+            service.DATA_SOURCE_USERS: test_users_repository,
             service.CONNECT: connection,
             service.SETUP: setup}
 
     production = {service.DB_CONFIGURATION: db_configuration,
-                  service.DATA_SOURCE: posts_repository,
+                  service.DATA_SOURCE_POSTS: posts_repository,
+                  service.DATA_SOURCE_USERS: users_repository,
                   service.CONNECT: connection,
                   service.SETUP: setup}
 

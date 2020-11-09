9d325b1832f270f5437ef29ceb6cf17d481ae373
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-09 10:12:56 +0200

    Added model class for users

---
+++ JMBlogWebApp/models/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__():
+        pass
+    self.user_id = id
+    self.user_name = name
+    self.user_email = email
+    self.user_password = password
+    self.user_created_at = created_at
+    self.user_modifed_at = modified_at

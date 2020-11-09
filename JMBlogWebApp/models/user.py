88cb03fe21019d62613488f28827350bb3aa5485
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-09 17:04:55 +0200

    Implemented uuid and creation/edit date with edit method in user

--- JMBlogWebApp/models/user.py
+++ JMBlogWebApp/models/user.py
@@ -1,8 +1,16 @@
+import uuid
+from models.timestamp import TimeStamp
+
 class User:
-    def __init__(self, user_id, user_name, user_email, user_password, user_created_at, user_modified_at):
-        self.user_id = user_id
+    def __init__(self, user_name, user_email, user_password):
+        self.user_id = uuid.uuid4()
+        self.user_name = user_name
+        self.user_email = user_email
+        self.user_password = user_password #temp: to hash
+        self.user_timestamp = TimeStamp()
+
+    def update(self, user_name, user_email, user_password):
         self.user_name = user_name
         self.user_email = user_email
         self.user_password = user_password
-        self.user_created_at = user_created_at
-        self.user_modifed_at = user_modified_at
+        self.stamp.on_edit()

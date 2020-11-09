69bf457745475c2109c23c4ddc1dccf888a16259
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-09 10:18:27 +0200

    added & registered user management blueprint

--- JMBlogWebApp/app.py
+++ JMBlogWebApp/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, redirect
 from views import post_manager
 from views import setup_manager
+from views import user_manager
 
 app = Flask(__name__, static_url_path="", static_folder="static")
 
 with app.app_context():
     app.register_blueprint(post_manager.post_manager)
     app.register_blueprint(setup_manager.setup_manager)
+    app.register_blueprint(user_manager.user_manager)
 
     @app.route('/')
     @app.route('/<path:path>')

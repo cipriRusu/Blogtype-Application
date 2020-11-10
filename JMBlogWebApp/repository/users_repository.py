9054dc70220a0791c9b3420f64996f02ad39b8c5
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-10 12:44:02 +0200

    added UsersRepository base class

---
+++ JMBlogWebApp/repository/users_repository.py
@@ -0,0 +1,31 @@
+import abc
+from abc import ABCMeta
+
+class UsersRepository(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, sub):
+        if cls is UsersRepository:
+            if any('__iter__' in Q.__dict__ for Q in sub.__mro__):
+                return True
+            return False
+        return NotImplemented
+
+    @abc.abstractmethod
+    def add_user(self, user):
+        pass
+
+    @abc.abstractclassmethod
+    def update_user(cls, user):
+        pass
+
+    @abc.abstractmethod
+    def get_users(self):
+        pass
+
+    @abc.abstractmethod
+    def get_user_by_id(self, user_id):
+        pass
+
+    @abc.abstractmethod
+    def remove_user(self, user_id):
+        pass

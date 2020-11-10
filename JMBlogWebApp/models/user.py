7be63e198a1808cfe5251bec0ebdaba2cb0d7b45
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-10 21:48:34 +0200

    added hashing for password

--- JMBlogWebApp/models/user.py
+++ JMBlogWebApp/models/user.py
@@ -1,12 +1,13 @@
 import uuid
 from models.timestamp import TimeStamp
+from models.password_hasher import PasswordHasher
 
 class User:
     def __init__(self, user_name, user_email, user_password):
         self.user_id = uuid.uuid4()
         self.user_name = user_name
         self.user_email = user_email
-        self.user_password = user_password #temp: to hash
+        self.user_password = PasswordHasher().GetHash(user_password)
         self.user_timestamp = TimeStamp()
 
     def update(self, user_name, user_email, user_password):

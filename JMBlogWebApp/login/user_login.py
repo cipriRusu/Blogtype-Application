47a29d18b420616eb29a444331fab4482a0d887c
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-13 11:33:54 +0200

    added login form routing to manager (& login validation)

---
+++ JMBlogWebApp/login/user_login.py
@@ -0,0 +1,22 @@
+from models.password_hasher import PasswordHasher
+
+class UserLogin:
+    def __init__(self, connection):
+        self._conn = connection
+        self._username = None
+        self._password_hash = None
+        self._query_result = None
+
+    def get_credentials(self, form_user, form_pass):
+        self._username = form_user
+        self._password_hash = PasswordHasher().get_hash(form_pass)
+
+    def is_login_valid(self):
+        self._conn.create_connection()
+        self._query_result = self._conn.execute("SELECT EXISTS\
+                                                (SELECT * FROM\
+                                                 users WHERE user_name =%s\
+                                                 AND user_password =%s)::BOOL",
+                                                (self._username,
+                                                 self._password_hash))
+        return self._query_result.fetchone()[0]

b7a068e2365819649c05329b6a48630d9f5027eb
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-13 11:59:33 +0200

    Injected login_service into login_manager: TODO: Implement login validation and handling

--- JMBlogWebApp/views/login_manager.py
+++ JMBlogWebApp/views/login_manager.py
@@ -1,18 +1,19 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for
+from setup import services_listing as services
 from decorators import decorators
 
-login_manager = Blueprint(
-    'login_manager',
-    __name__,
-    url_prefix='/login',
-    template_folder='templates')
+login_manager = Blueprint('login_manager', __name__, url_prefix='/login',
+                          template_folder='templates')
 
 @login_manager.route('/', methods=["GET", "POST"])
 @decorators.config_check
 @decorators.inject
-def login():
-    #TODO: Inject login manager?
+def login(login_service: services.USER_LOGIN):
     if request.method == "POST":
-        session['logged'] = request.form['NameInput']
+        login_service.get_credentials(
+            request.form['NameInput'],
+            request.form['PasswordInput'])
+        #TODO: Implement login validation & session
+        login_service.is_login_valid()
         return redirect(url_for('post_manager.index'))
     return render_template("login_user.html")

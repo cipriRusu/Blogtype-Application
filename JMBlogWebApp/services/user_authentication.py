e68968f92cf2c9b4bb10246c2e74b47ec77e1445
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-14 17:58:42 +0200

    Implemented basic login method and session assignment for user_name and user_id

--- JMBlogWebApp/services/user_authentication.py
+++ JMBlogWebApp/services/user_authentication.py
@@ -1,22 +1,21 @@
+from flask import session
 from services.password_hasher import PasswordHasher
 
-class UserAuthentication:
-    def __init__(self, connection):
-        self._conn = connection
+class UserAuthentication():
+    def __init__(self, users_db):
+        self._db = users_db
         self._username = None
         self._password_hash = None
-        self._query_result = None
+        self._found_user = None
 
-    def get_credentials(self, form_user, form_pass):
+    def user_login(self, form_user, form_pass):
         self._username = form_user
         self._password_hash = PasswordHasher().get_hash(form_pass)
+        self._found_user = self._db.get_by_name_and_pass(self._username,
+                                                         self._password_hash)
 
-    def is_login_valid(self):
-        self._conn.create_connection()
-        self._query_result = self._conn.execute("SELECT EXISTS\
-                                                (SELECT * FROM\
-                                                 users WHERE user_name =%s\
-                                                 AND user_password =%s)::BOOL",
-                                                (self._username,
-                                                 self._password_hash))
-        return self._query_result.fetchone()[0]
+        if self._found_user is not None:
+            session['logged_name'] = self._found_user.user_name
+            session['logged_id'] = self._found_user.user_id
+            return True
+        return False

2b30aadf61885c1d3fb9bb40056e94c67265152d
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-14 17:56:14 +0200

    Added test login service and implemented basic login in test mode

--- JMBlogWebApp/services/services.py
+++ JMBlogWebApp/services/services.py
@@ -11,32 +11,33 @@ from repository.in_memory_data import in_memory_users as test_data_users
 from services.user_authentication import UserAuthentication
 
 class Services():
-    IS_TEST = False
+    IS_TEST = True
     def __init__(self):
         pass
 
     db_configuration = DbConfig()
     connection = DbConnect(db_configuration)
     setup = DbSetup(connection)
-    login = UserAuthentication(connection)
     test_users_repository = UsersInMemoryRepository(test_data_users)
     test_posts_repository = PostsInMemoryRepository(test_data_posts)
     posts_repository = PostsDBRepository(connection)
     users_repository = UsersDBRepository(connection)
+    database_login = UserAuthentication(users_repository)
+    test_login = UserAuthentication(test_users_repository)
 
     test = {service.DB_CONFIGURATION: db_configuration,
             service.DATA_SOURCE_POSTS: test_posts_repository,
             service.DATA_SOURCE_USERS: test_users_repository,
             service.CONNECT: connection,
             service.SETUP: setup,
-            service.USER_LOGIN: login}
+            service.USER_LOGIN: test_login}
 
     production = {service.DB_CONFIGURATION: db_configuration,
                   service.DATA_SOURCE_POSTS: posts_repository,
                   service.DATA_SOURCE_USERS: users_repository,
                   service.CONNECT: connection,
                   service.SETUP: setup,
-                  service.USER_LOGIN: login}
+                  service.USER_LOGIN: database_login}
 
     @classmethod
     def is_service(cls, service_name):

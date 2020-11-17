f7428f50343e44f990d846dd31c142bfc37acfa7
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-17 17:36:18 +0200

    Changed timestamp formatting

--- JMBlogWebApp/models/timestamp.py
+++ JMBlogWebApp/models/timestamp.py
@@ -2,8 +2,8 @@ import datetime
 
 class TimeStamp:
     def __init__(self):
-        self.creation_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
+        self.creation_time = datetime.datetime.now().strftime("%Y-%B-%d, %H:%M:%S")
         self.edit_time = None
 
     def on_edit(self):
-        self.edit_time = datetime.datetime.now().strftime("%Y-%B-%d %H:%M:%S")
+        self.edit_time = datetime.datetime.now().strftime("%Y-%B-%d, %H:%M:%S")

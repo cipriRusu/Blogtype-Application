24081febe36042e83aec7dc5edce0f17d297dff1
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-17 17:36:42 +0200

    Added specific constraints for inner joining

--- JMBlogWebApp/setup/postgres_scripts.py
+++ JMBlogWebApp/setup/postgres_scripts.py
@@ -4,17 +4,17 @@ LIST_DATABASES_SCRIPT = "SELECT datname from pg_database"
 
 SEARCH_TABLE_SCRIPT = "SELECT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME=%s)"
 
-CREATE_TABLE_SCRIPT = "CREATE TABLE POSTS(posts_id uuid,\
+CREATE_TABLE_SCRIPT = "CREATE TABLE POSTS(posts_id uuid UNIQUE,\
 creation_date timestamp,\
 edit_date timestamp,\
-author uuid,\
+author uuid UNIQUE,\
 title varchar,\
 post_content varchar,\
 PRIMARY KEY(posts_id),\
 FOREIGN KEY(author)\
     REFERENCES users(user_id));"
 
-CREATE_USERS_SCRIPT = "CREATE TABLE USERS(user_id uuid,\
+CREATE_USERS_SCRIPT = "CREATE TABLE USERS(user_id uuid UNIQUE,\
 user_name varchar UNIQUE,\
 user_email varchar,\
 user_password varchar,\

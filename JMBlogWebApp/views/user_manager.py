f36b6c23e36663d1cc7353103360b4be5d7d5e93
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-17 15:31:25 +0200

    Added appropriate decorators for particular routes

--- JMBlogWebApp/views/user_manager.py
+++ JMBlogWebApp/views/user_manager.py
@@ -12,26 +12,28 @@ user_manager = Blueprint(
 @user_manager.route('/', methods=["GET"])
 @decorators.config_check
 @decorators.inject
-def index(users_database: services.DATA_SOURCE_USERS):
-    return render_template("list_users.html", database=users_database.get_users())
+@decorators.requires_admin
+def index(current_database: services.DATA_SOURCE_USERS):
+    return render_template("list_users.html", database=current_database.get_users())
 
 @user_manager.route('/<uuid:user_index>')
 @decorators.config_check
 @decorators.inject
-def content(user_index, current: services.DATA_SOURCE_USERS):
-    return render_template("view_user.html", element=current.get_user_by_id(user_index))
+@decorators.admin_or_owner_required
+def content(user_index, current_database: services.DATA_SOURCE_USERS):
+    return render_template("view_user.html", element=current_database.get_user_by_id(user_index))
 
 @user_manager.route('/add', methods=["GET", "POST"])
 @decorators.config_check
 @decorators.inject
 @decorators.requires_admin
-def add_item(users_database: services.DATA_SOURCE_USERS):
+def add_item(current_database: services.DATA_SOURCE_USERS):
     if request.method == "POST":
         to_add = User(
             request.form['NameInput'],
             request.form['EmailInput'],
             request.form['PasswordInput'])
-        users_database.add_user(to_add)
+        current_database.add_user(to_add)
         return redirect(url_for('.content', user_index=to_add.user_id))
     return render_template("add_user.html")
 
@@ -46,7 +48,7 @@ def remove_item(user_index, current_database: services.DATA_SOURCE_USERS):
 @user_manager.route('/update/<uuid:user_index>', methods=["GET", "POST"])
 @decorators.config_check
 @decorators.inject
-@decorators.requres_login
+@decorators.admin_or_owner_required
 def update_item(user_index, current_database: services.DATA_SOURCE_USERS):
     if request.method == "GET":
         return render_template("update_user.html",

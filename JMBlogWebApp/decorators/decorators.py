1e90a0e97b01d0c2e7bd770d1ea144df5c6878e2
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-19 05:53:02 -0800

    Handled unlogged branches (for user_manager) 	modified:   decorators/decorators.py

--- JMBlogWebApp/decorators/decorators.py
+++ JMBlogWebApp/decorators/decorators.py
@@ -45,14 +45,17 @@ def admin_or_owner_required(func):
         if 'post_index' in kwargs:
             found_post = kwargs['current_database'].get_by_id(kwargs['post_index'])
             if 'logged_name' in session:
-                if found_post.author == session['logged_name'] or session['logged_name'] == 'admin':
+                if (found_post.author == session['logged_name'] or
+                        session['logged_name'] == 'admin'):
                     return func(*args, **kwargs)
+                return redirect(url_for('error_manager.error_redirect'))
             return redirect(url_for('error_manager.error_redirect'))
 
         if 'user_index' in kwargs:
             found_user = kwargs['current_database'].get_user_by_id(kwargs['user_index'])
             if 'logged_name' in session:
-                if found_user.user_name == session['logged_name'] or session['logged_name'] == 'admin':
+                if (found_user.user_name == session['logged_name'] or
+                        session['logged_name'] == 'admin'):
                     return func(*args, **kwargs)
                 return redirect(url_for('error_manager.error_redirect'))
             return redirect(url_for('error_manager.error_redirect'))

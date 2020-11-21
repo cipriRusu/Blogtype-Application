e082ac1f90d96a8143589e06407c6aea4a09bffb
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-22 01:14:24 +0200

    Renamed 'create_connection' to 'establish_connection' and added update database handling for no initial setup (TODO: include all cases) 	modified:   setup/db_setup.py

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,13 +1,12 @@
 from setup.config import Config
 import setup.postgres_scripts as scripts
 
-
 class DbSetup():
-    CURRENT_DB_VERSION = 0
     def __init__(self, db_connection):
         self._connection = db_connection
+        self._db_version = 0
 
-    def create_database(self):
+    def establish_connection(self):
         try:
             self._connection.create_connection()
         except:
@@ -15,11 +14,26 @@ class DbSetup():
                             "Connection cannot be established\
                              as there is no local available database")
 
-        if not self._connection.contains_table():
-            self._connection.execute(scripts.CREATE_TABLE_SCRIPT)
-
         self._connection.close_connection()
 
     def update_database(self):
-        res = DbSetup.CURRENT_DB_VERSION
-        pass
+        current_configuration = Config().from_file('db_connection')
+
+        if self._db_version == 0:
+            self._connection.create_host_connection()
+
+            if not self._connection.contains_db(current_configuration['database']):
+                self._connection.execute(scripts.CREATE_DATABASE_SCRIPT %
+                                         current_configuration['database'])
+                self._connection.close_connection()
+
+                self._connection.create_connection()
+                self._connection.execute(scripts.CREATE_USERS_SCRIPT)
+                self._connection.execute(scripts.CREATE_POSTS_SCRIPT)
+                self._connection.execute(scripts.CREATE_ADMIN_SCRIPT)
+                self._connection.close_connection()
+
+    def set_version(self):
+        if Config().is_configured():
+            if Config().has_section('db_version'):
+                self._db_version = int(Config().from_file('db_version')['version'])

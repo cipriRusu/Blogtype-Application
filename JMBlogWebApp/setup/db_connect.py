87efddf49253c4b90fd7439d9df73fcc56cb1c4b
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 00:34:49 +0200

    Removed host connection method from DbConnect 	modified:   setup/db_connect.py

--- JMBlogWebApp/setup/db_connect.py
+++ JMBlogWebApp/setup/db_connect.py
@@ -14,18 +14,8 @@ class DbConnect():
         self._connection.autocommit = True
         self._cursor = self._connection.cursor()
 
-    def create_host_connection(self):
-        self._loaded_config = self._config.load_configuration()
-        self._connection = psycopg2.connect(**self._loaded_config.get_dictionary('database'))
-        self._connection.autocommit = True
-        self._cursor = self._connection.cursor()
-
     def contains_table(self, table_name):
         self._cursor.execute(scripts.SEARCH_TABLE_SCRIPT % table_name)
-        return self._cursor.fetchone()[0]
-
-    def contains_db(self, db_name):
-        self._cursor.execute(scripts.SEARCH_DATABASE_SCRIPT % db_name)
         return self._cursor.fetchone()[0]
 
     def close_connection(self):

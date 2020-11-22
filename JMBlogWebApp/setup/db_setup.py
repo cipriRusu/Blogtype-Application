0014b9f7621db29439156a056dd4ae4035cdea3b
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 00:47:36 +0200

    Added basic update_database logic 	modified:   setup/db_setup.py

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,4 +1,3 @@
-from setup.config import Config
 import setup.postgres_scripts as scripts
 
 class DbSetup():
@@ -6,7 +5,7 @@ class DbSetup():
         self._connection = db_connection
         self._db_version = 0
 
-    def establish_connection(self):
+    def create_db(self):
         try:
             self._connection.create_connection()
         except:
@@ -17,23 +16,10 @@ class DbSetup():
         self._connection.close_connection()
 
     def update_database(self):
-        current_configuration = Config().from_file('db_connection')
+        self._connection.create_connection()
+        for script in scripts.CREATE_FULL_DATABASE:
+            self._connection.execute(script)
+        self._connection.close_connection()
 
-        if self._db_version == 0:
-            self._connection.create_host_connection()
-
-            if not self._connection.contains_db(current_configuration['database']):
-                self._connection.execute(scripts.CREATE_DATABASE_SCRIPT %
-                                         current_configuration['database'])
-                self._connection.close_connection()
-
-                self._connection.create_connection()
-                self._connection.execute(scripts.CREATE_USERS_SCRIPT)
-                self._connection.execute(scripts.CREATE_POSTS_SCRIPT)
-                self._connection.execute(scripts.CREATE_ADMIN_SCRIPT)
-                self._connection.close_connection()
-
-    def set_version(self):
-        if Config().is_configured():
-            if Config().has_section('db_version'):
-                self._db_version = int(Config().from_file('db_version')['version'])
+    def is_db_outdated(self):
+        pass

22454ab1d0b25b30ae49911a369bb2fbef7f6a76
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 00:36:12 +0200

    Added methods for returning db version and setting db version from config class
    Added default configuration returning if no config available
    	modified:   setup/db_config.py

--- JMBlogWebApp/setup/db_config.py
+++ JMBlogWebApp/setup/db_config.py
@@ -1,14 +1,25 @@
 from models.database_configuration import DatabaseConfiguration
 from setup.config import Config
+from setup.db_default import DefaultConfiguration
 
 class DbConfig(Config):
     DB_CONNECTION_HEADER = 'db_connection'
+    DB_VERSION_HEADER = 'db_version'
 
     def load_configuration(self):
-        return DatabaseConfiguration(**super().from_file(DbConfig.DB_CONNECTION_HEADER))
+        if self.is_configured():
+            return DatabaseConfiguration(**super().from_file(DbConfig.DB_CONNECTION_HEADER))
+        return DefaultConfiguration()
 
     def save_configuration(self, received_stats):
         super().to_file(received_stats, DbConfig.DB_CONNECTION_HEADER)
 
     def is_configured(self):
         return super().is_configured() and super().has_section(DbConfig.DB_CONNECTION_HEADER)
+
+    def get_db_version(self):
+        if super().is_configured() and super().has_section(DbConfig.DB_VERSION_HEADER):
+            return super().from_file(DbConfig.DB_VERSION_HEADER)
+
+    def set_db_version(self, updated_version):
+        super().to_file(updated_version, DbConfig.DB_VERSION_HEADER)

f5f1a61bd929da3734d142ebc003218f26cff7a7
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 18:59:06 +0200

    Added version setting and complete database creation from scratch(TODO: update if database already exists) 	modified:   setup/db_setup.py

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -2,6 +2,7 @@ from models.database_version import DatabaseVersion
 import setup.postgres_scripts as scripts
 
 class DbSetup():
+    LATEST_VERSION = 3
     def __init__(self, db_connection):
         self._connection = db_connection
         self._configuration = db_connection._config
@@ -14,23 +15,15 @@ class DbSetup():
             raise Exception(self._connection,
                             "Connection cannot be established\
                              as there is no local available database")
-
-        if not self._connection.contains_table():
-            for script in scripts.CREATE_FULL_DATABASE:
-                self._connection.execute(script)
+        
+        for script in scripts.CREATE_FULL_DATABASE:
+            self._connection.execute(script)
 
         self._connection.close_connection()
 
     def update_database(self):
-        try:
-            self._connection.create_connection()
-            ##TODO:UPDATE IF NEEDED##
-            self._connection.close_connection()
-        except:
-            ##TODO:CREATE DB FROM SCRATCH##
-            pass
-
+        self._connection.create_db()
         self._configuration.set_db_version(DatabaseVersion(3))
 
     def is_db_outdated(self):
-        return self._configuration.get_db_version() != 3
+        return self._configuration.get_db_version() != DbSetup.LATEST_VERSION

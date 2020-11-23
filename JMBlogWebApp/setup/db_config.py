b7de0727e1a29a56527cb4807f38fa59456a630a
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 07:11:39 +0200

    Removed default configuration (in case of no present config) Added database version casting (Returns integer as db version) 	modified:   setup/db_config.py

--- JMBlogWebApp/setup/db_config.py
+++ JMBlogWebApp/setup/db_config.py
@@ -1,15 +1,12 @@
 from models.database_configuration import DatabaseConfiguration
 from setup.config import Config
-from setup.db_default import DefaultConfiguration
 
 class DbConfig(Config):
     DB_CONNECTION_HEADER = 'db_connection'
     DB_VERSION_HEADER = 'db_version'
 
     def load_configuration(self):
-        if self.is_configured():
-            return DatabaseConfiguration(**super().from_file(DbConfig.DB_CONNECTION_HEADER))
-        return DefaultConfiguration()
+        return DatabaseConfiguration(**super().from_file(DbConfig.DB_CONNECTION_HEADER))
 
     def save_configuration(self, received_stats):
         super().to_file(received_stats, DbConfig.DB_CONNECTION_HEADER)
@@ -19,7 +16,7 @@ class DbConfig(Config):
 
     def get_db_version(self):
         if super().is_configured() and super().has_section(DbConfig.DB_VERSION_HEADER):
-            return super().from_file(DbConfig.DB_VERSION_HEADER)
+            return int(super().from_file(DbConfig.DB_VERSION_HEADER)['version'])
         return 0
 
     def set_db_version(self, updated_version):

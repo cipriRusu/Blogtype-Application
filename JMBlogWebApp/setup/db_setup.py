303bbe7b7f999352a0d77f0eb27f71d356f51cf1
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-23 03:01:16 +0200

    Added database versioning loading and saving 	modified:   setup/db_setup.py

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,8 +1,10 @@
+from models.database_version import DatabaseVersion
 import setup.postgres_scripts as scripts
 
 class DbSetup():
     def __init__(self, db_connection):
         self._connection = db_connection
+        self._configuration = db_connection._config
         self._db_version = 0
 
     def create_db(self):
@@ -17,9 +19,13 @@ class DbSetup():
 
     def update_database(self):
         self._connection.create_connection()
+
         for script in scripts.CREATE_FULL_DATABASE:
             self._connection.execute(script)
+
         self._connection.close_connection()
 
+        self._configuration.set_db_version(DatabaseVersion(3))
+
     def is_db_outdated(self):
-        pass
+        return self._configuration.get_db_version() != 3

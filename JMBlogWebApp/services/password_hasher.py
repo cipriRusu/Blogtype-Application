319535ca085685e244067402d01bc0d05e0deac1
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-26 15:14:39 +0200

    Added empty return value (for empty password in database) 	modified:   services/password_hasher.py

--- JMBlogWebApp/services/password_hasher.py
+++ JMBlogWebApp/services/password_hasher.py
@@ -6,4 +6,6 @@ class PasswordHasher:
 
     @classmethod
     def get_hash(cls, plain_text):
+        if not plain_text:
+            return ''
         return hashlib.sha256(plain_text.encode('utf-8')).hexdigest()

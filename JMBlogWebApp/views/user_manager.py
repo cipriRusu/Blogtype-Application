1238e7ae7329e58da1b40c514c7ef40e7f010b0e
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-26 16:07:43 +0200

    Added route for resetting password 	modified:   views/user_manager.py

--- JMBlogWebApp/views/user_manager.py
+++ JMBlogWebApp/views/user_manager.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from setup import services_listing as services
 from models.user import User
 from views.decorators import setup_decorators
@@ -58,6 +58,30 @@ def update_item(user_index, current_database: services.DATA_SOURCE_USERS):
 
     if request.method == "POST":
         current = current_database.get_user_by_id(user_index)
+        current.update(request.form['UserNameInput'],
+                       request.form['UserEmailInput'],
+                       request.form['UserPassInput'])
+        current_database.update_user(current)
+        return redirect(url_for('.content', user_index=current.user_id))
+    return Exception("Request type cannot be handled")
+
+@user_manager.route('/reset/<uuid:user_index>', methods=["GET", "POST"])
+@setup_decorators.config_check
+@inject_decorators.inject
+@authorization_decorators.admin_or_owner_required
+def reset_item(user_index, current_database: services.DATA_SOURCE_USERS):
+    if request.method == "GET":
+        return render_template("update_user.html",
+                               current=current_database.get_user_by_id(user_index))
+
+    if request.method == "POST":
+        current = current_database.get_user_by_id(user_index)
+
+        if not request.form['UserPassInput']:
+            flash('Invalid password')
+            return render_template("update_user.html",
+                                   current=current_database.get_user_by_id(user_index))
+
         current.update(request.form['UserNameInput'],
                        request.form['UserEmailInput'],
                        request.form['UserPassInput'])

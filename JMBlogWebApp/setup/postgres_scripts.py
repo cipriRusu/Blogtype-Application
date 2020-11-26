35267e2d0c6d884b6572e666d10dcec37b32d5a2
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-26 15:56:30 +0200

    Implemented rest of queries (for handling old database version/data migration) 	modified:   setup/postgres_scripts.py

--- JMBlogWebApp/setup/postgres_scripts.py
+++ JMBlogWebApp/setup/postgres_scripts.py
@@ -1,13 +1,6 @@
-LIST_DATABASES_SCRIPT = "SELECT datname FROM pg_database"
+ID_GENERATOR_SCRIPT = "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\""
 
-SEARCH_DATABASE_SCRIPT = "SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database \
-WHERE lower(datname) = lower('%s'))::bool"
-
-SEARCH_TABLE_SCRIPT = "SELECT EXISTS(SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='%s')"
-
-CREATE_DATABASE_SCRIPT = "CREATE DATABASE posts"
-
-#CREATE_DATABASE_SCRIPT = "CREATE DATABASE IF NOT EXISTS posts" #invalid syntax?
+CREATE_DATABASE_SCRIPT = "CREATE DATABASE {}"
 
 CREATE_USERS_SCRIPT = "CREATE TABLE IF NOT EXISTS users(user_id uuid UNIQUE,\
 user_name varchar UNIQUE,\
@@ -16,10 +9,19 @@ user_password varchar,\
 user_created_at timestamp,\
 user_modified_at timestamp);"
 
+TRANSFER_USERS_SCRIPT = "insert into users(user_id, user_name) \
+select distinct gen_random_uuid(), author from posts"
+
+UPDATE_POST_AUTHORS_COLUMN = "update posts set \
+author = users.user_id from users where users.user_name = posts.author"
+
+UPDATE_POST_AUTHORS_DATATYPE = "alter table posts \
+alter column author type uuid using author::uuid"
+
 CREATE_POSTS_SCRIPT = "CREATE TABLE IF NOT EXISTS posts(posts_id uuid UNIQUE,\
 creation_date timestamp,\
 edit_date timestamp,\
-author uuid UNIQUE,\
+author uuid not null,\
 title varchar,\
 post_content varchar,\
 PRIMARY KEY(posts_id),\
@@ -28,10 +30,15 @@ FOREIGN KEY(author)\
 
 CREATE_ADMIN_SCRIPT = "INSERT INTO USERS(user_id, user_name, user_password)\
 VALUES ('45137158-83d4-4871-9b50-2df81746cf29',\
-'admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918')"
+'admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918') ON CONFLICT DO NOTHING"
 
-CHECK_AUTHOR_SCRIPT = "SELECT DATATYPE FROM INFORMATION_SCHEMA.COLUMNS\
-WHERE TABLE_NAME='posts' AND COLUMN_NAME='author'"
+CREATE_ALL_DB_RELATIONS = [CREATE_USERS_SCRIPT,
+                           CREATE_POSTS_SCRIPT,
+                           CREATE_ADMIN_SCRIPT]
 
-CREATE_FULL_DATABASE = [CREATE_DATABASE_SCRIPT, CREATE_USERS_SCRIPT,\
-                        CREATE_POSTS_SCRIPT, CREATE_ADMIN_SCRIPT]
+UPDATE_EXISTING_DB = [ID_GENERATOR_SCRIPT,
+                      CREATE_USERS_SCRIPT,
+                      CREATE_ADMIN_SCRIPT,
+                      TRANSFER_USERS_SCRIPT,
+                      UPDATE_POST_AUTHORS_COLUMN,
+                      UPDATE_POST_AUTHORS_DATATYPE]

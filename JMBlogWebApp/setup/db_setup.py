7f3f2fa28908360d430a505ca3638fd71f309d11
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-29 13:10:10 +0200

    Added setup methods (create database if not present and updating if necessary) 	modified:   setup/db_setup.py

--- JMBlogWebApp/setup/db_setup.py
+++ JMBlogWebApp/setup/db_setup.py
@@ -1,33 +1,40 @@
+import psycopg2
 import setup.postgres_scripts as scripts
+from models.database_version import DatabaseVersion
 
 class DbSetup():
     LATEST_VERSION = 3
+
     def __init__(self, db_connection):
         self._connection = db_connection
         self._configuration = db_connection._config
         self._db_version = 0
+        self._params = None
 
     def create_db(self):
+        _params = self._configuration.load_configuration().get_dictionary()
+        _conn = psycopg2.connect(**{x: _params[x] for x in ['host', 'user', 'password']})
+        _conn.autocommit = True
+        _cursor = _conn.cursor()
+        _cursor.execute('''CREATE DATABASE {}'''.format(_params['database']))
+        _cursor.close()
+        _conn.close()
+
+    def update_db(self):
+        if self._configuration.is_configured():
+            if self._configuration.get_db_version() != DbSetup.LATEST_VERSION:
+                self._connection.create_connection()
+                for script in scripts.ALL_SCRIPTS:
+                    self._connection.execute(script)
+                self._connection.close_connection()
+        self._configuration.set_db_version(DatabaseVersion(3))
+
+    def setup(self):
         try:
-            self._connection.create_connection()
-        except:
-            raise Exception(self._connection,
-                            "Connection cannot be established\
-                             as there is no local available database")
-
-        if self._connection.can_update():
-            self.update_database()
-
-        for script in scripts.CREATE_ALL_DB_RELATIONS:
-            self._connection.execute(script)
-
-        self._connection.close_connection()
-
-    def update_database(self):
-        try:
-            self._connection.create_db()
-        except:
-            self._connection.update_db()
+            self.create_db()
+        except psycopg2.DatabaseError:
+            pass
+        self.update_db()
 
     def is_db_outdated(self):
         return self._configuration.get_db_version() != DbSetup.LATEST_VERSION

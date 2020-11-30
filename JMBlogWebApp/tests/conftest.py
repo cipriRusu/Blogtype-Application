d45ca2270019ea7908a9ae2925546480962bec52
Cipri Rusu <cipri91rusu@gmail.com>
2020-11-30 20:40:24 +0200

    Added scope for conftest: TODO: figure out local 	modified:   tests/conftest.py

--- JMBlogWebApp/tests/conftest.py
+++ JMBlogWebApp/tests/conftest.py
@@ -6,7 +6,7 @@ from services.services import Services
 Services.IS_TEST = True
 
 @pytest.fixture
-def configured_app():
+def configured_app(scope='function'):
     with patch('setup.db_config.DbConfig.is_configured', return_value=True):
         with patch('setup.db_setup.DbSetup.is_db_outdated', return_value=False):
             response_from = app.test_client()

6219a03ead811c665301309a5b3d215f97f67483
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-02 09:50:07 +0200

    Added parameter into user model (for storing former user_name) for edit 	modified:   models/user.py

--- JMBlogWebApp/models/user.py
+++ JMBlogWebApp/models/user.py
@@ -9,8 +9,10 @@ class User:
         self.user_email = user_email
         self.user_password = PasswordHasher().get_hash(user_password)
         self.user_timestamp = TimeStamp()
+        self.user_old_name = None
 
     def update(self, user_name, user_email, user_password):
+        self.user_old_name = self.user_name
         self.user_name = user_name
         self.user_email = user_email
         self.user_password = PasswordHasher().get_hash(user_password)

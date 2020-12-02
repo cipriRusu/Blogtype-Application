e16b1d4ba9ce4569cc93a0b66714490fe846539e
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-02 09:52:37 +0200

    Added parameter values for in memory element 	modified:   repository/in_memory_data.py

--- JMBlogWebApp/repository/in_memory_data.py
+++ JMBlogWebApp/repository/in_memory_data.py
@@ -57,3 +57,5 @@ in_memory_users[1].user_id = uuid.UUID('6ee39856-2721-46c4-bda7-3faf8e4a60f5')
 in_memory_users[1].user_timestamp.creation_time = '2015-03-12 12:02:31'
 in_memory_users[2].user_id = uuid.UUID('25447284-aa74-4fb6-b7a0-2bb955f2b2b1')
 in_memory_users[2].user_timestamp.creation_time = '2009-06-15 13:21:35'
+in_memory_users[3].user_id = uuid.UUID('99ae0e65-372b-4f4a-be88-776d6a4d92bd')
+in_memory_users[3].user_timestamp.creation_time = '2009-06-15 13:21:35'

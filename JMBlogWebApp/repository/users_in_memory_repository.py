3ec6a591ed8ef883ddacbc4ca08adc2ee244a902
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-05 10:40:32 -0800

    Removed old_name checking from repository 	modified:   repository/users_in_memory_repository.py

--- JMBlogWebApp/repository/users_in_memory_repository.py
+++ JMBlogWebApp/repository/users_in_memory_repository.py
@@ -16,10 +16,6 @@ class UsersInMemoryRepository(UsersRepository):
         self._db.remove(user)
         self._db.append(user)
 
-        for post in in_memory_posts:
-            if post.author == user.user_old_name:
-                post.author = user.user_name
-
     def get_users(self):
         return self._db
 

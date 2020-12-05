36c8861c03f92f2817694fafe12a0f66bce8501e
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-05 15:41:21 -0800

    Refactored Update method (simplified implementation and fixed edit_date not displayed)
    Added edit_date into all blogposts
    	modified:   repository/posts_in_memory_repository.py

--- JMBlogWebApp/repository/posts_in_memory_repository.py
+++ JMBlogWebApp/repository/posts_in_memory_repository.py
@@ -7,8 +7,8 @@ class PostsInMemoryRepository(PostsRepository):
         self._users_db = users_db
 
     def __iter__(self):
-        for element in self._db:
-            yield element
+        for post in self._db:
+            yield post
 
     def add_post(self, item):
         for user in self._users_db:
@@ -16,16 +16,11 @@ class PostsInMemoryRepository(PostsRepository):
                 self._db.append(item)
 
     def update_post(self, item):
-        for initial_post in self._db:
-            if initial_post.post_id == item.post_id:
-                updated_post = BlogPost(
-                    item.title,
-                    self._users_db.get_by_name(item.author).user_id,
-                    item.content)
-
-                updated_post.post_id = item.post_id
-
-                self._db.remove(initial_post)
+        for found_post in self._db:
+            if found_post.post_id == item.post_id:
+                updated_post = found_post
+                self._db.remove(found_post)
+                updated_post.update(item.title, item.content)
                 self._db.append(updated_post)
 
     def get_by_id(self, index):
@@ -36,26 +31,29 @@ class PostsInMemoryRepository(PostsRepository):
                     self._users_db.get_user_by_id(element.author).user_name,
                     element.content)
 
+                found_post.stamp.creation_time = element.stamp.creation_time
+                found_post.stamp.edit_time = element.stamp.edit_time
                 found_post.post_id = element.post_id
                 return found_post
-
         raise Exception("no element found!")
 
     def get_all(self):
         all_posts = []
 
         for post in self._db:
-            element = BlogPost(
+            found_post = BlogPost(
                 post.title,
                 self._users_db.get_user_by_id(post.author).user_name,
                 post.content)
 
-            element.post_id = post.post_id
-            all_posts.append(element)
+            found_post.stamp.creation_time = post.stamp.creation_time
+            found_post.stamp.edit_time = post.stamp.edit_time
+            found_post.post_id = post.post_id
+            all_posts.append(found_post)
 
         return all_posts
 
     def remove(self, index):
-        for element in self._db:
-            if element.post_id == index:
-                self._db.remove(element)
+        for found_post in self._db:
+            if found_post.post_id == index:
+                self._db.remove(found_post)

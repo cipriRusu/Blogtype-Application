8a032bf3a34528cc48d248ac10c3567074348179
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-05 21:50:21 -0800

    Added queryString "User" handling into indexer modified:   views/post_manager.py

--- JMBlogWebApp/views/post_manager.py
+++ JMBlogWebApp/views/post_manager.py
@@ -10,8 +10,14 @@ post_manager = Blueprint('post_manager', __name__, url_prefix='/posts', template
 @post_manager.route('/', methods=["GET"])
 @setup_decorators.config_check
 @inject_decorators.inject
-def index(current_database: services.DATA_SOURCE_POSTS):
-    return render_template("list_posts.html", database=current_database.get_all())
+def index(current_database: services.DATA_SOURCE_POSTS,
+          current_users: services.DATA_SOURCE_USERS):
+
+    username_filter = None if "Users" not in request.args else request.args["Users"]
+
+    return render_template("list_posts.html",
+                           database=current_database.get_all(filter_by=username_filter),
+                           users=current_users.get_users())
 
 @post_manager.route('/<uuid:post_index>')
 @setup_decorators.config_check

e4a0fa48ff9eaeb48ee1e1d5b802ce956c80efa5
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-09 23:58:47 -0800

    Adjusted tests for pagination

--- JMBlogWebApp/tests/test_login_manager.py
+++ JMBlogWebApp/tests/test_login_manager.py
@@ -4,15 +4,19 @@ def test_unlogged_basic_login(configured_app):
 
 def test_login_for_admin(configured_app):
     login_data = {"NameInput": "admin", "PasswordInput": "adminpass"}
-    current = configured_app.post('/authentication/login', data=login_data,
-                                  follow_redirects=True).data
-    assert b'FirstTitle' in current
+    configured_app.post('/authentication/login', data=login_data,
+                        follow_redirects=True)
+
+    after_login_page = configured_app.get('/posts/?Page=0').data
+    assert b'SecondTitle' in after_login_page
 
 def test_redirect_after_correct_user_login(configured_app):
     login_data = {"NameInput": "FirstAuthor", "PasswordInput": "fpass"}
-    current = configured_app.post('/authentication/login', data=login_data,
-                                  follow_redirects=True).data
-    assert b'FirstTitle' in current
+    configured_app.post('/authentication/login', data=login_data,
+                        follow_redirects=True)
+
+    after_login_page = configured_app.get('/posts/?Page=0').data
+    assert b'SecondTitle' in after_login_page
 
 def test_redirect_after_incorrect_user_login(configured_app):
     login_data = {"NameInput": "FirstAuthor", "PasswordInput": "wrongpass"}

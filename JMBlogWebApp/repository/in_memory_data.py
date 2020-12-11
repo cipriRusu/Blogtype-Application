31d1109330a20a19dbf465ae7385e226d69c2e18
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-11 08:14:23 -0800

    Added extra posts for pagination testing 	modified:   repository/in_memory_data.py

--- JMBlogWebApp/repository/in_memory_data.py
+++ JMBlogWebApp/repository/in_memory_data.py
@@ -81,6 +81,76 @@ in_memory_posts = [BlogPost("FirstTitle", uuid.UUID('6ee39856-2721-46c4-bda7-3fa
                               Latin words, combined with a handful of model sentence structures,\
                               to generate Lorem Ipsum which looks reasonable. The generated Lorem \
                               Ipsum is therefore always free from repetition, injected humour, \
+                              or non-characteristic words etc."),
+
+                    BlogPost("SeventhTitle", uuid.UUID('99ae0e65-372b-4f4a-be88-776d6a4d92bd')\
+                              ,"There are many variations of passages of Lorem Ipsum available,\
+                              but the majority have suffered alteration in some form, by injected\
+                              humour, or randomised words which don't look even slightly believable. \
+                              If you are going to use a passage of Lorem Ipsum, you need to\
+                              be sure there isn't anything embarrassing hidden in the middle\
+                              of text. All the Lorem Ipsum generators on the Internet tend to\
+                              repeat predefined chunks as necessary, making this the first \
+                              true generator on the Internet. It uses a dictionary of over 200\
+                              Latin words, combined with a handful of model sentence structures,\
+                              to generate Lorem Ipsum which looks reasonable. The generated Lorem \
+                              Ipsum is therefore always free from repetition, injected humour, \
+                              or non-characteristic words etc."),
+
+                    BlogPost("EightTitle", uuid.UUID('6ee39856-2721-46c4-bda7-3faf8e4a60f5')\
+                              ,"There are many variations of passages of Lorem Ipsum available,\
+                              but the majority have suffered alteration in some form, by injected\
+                              humour, or randomised words which don't look even slightly believable. \
+                              If you are going to use a passage of Lorem Ipsum, you need to\
+                              be sure there isn't anything embarrassing hidden in the middle\
+                              of text. All the Lorem Ipsum generators on the Internet tend to\
+                              repeat predefined chunks as necessary, making this the first \
+                              true generator on the Internet. It uses a dictionary of over 200\
+                              Latin words, combined with a handful of model sentence structures,\
+                              to generate Lorem Ipsum which looks reasonable. The generated Lorem \
+                              Ipsum is therefore always free from repetition, injected humour, \
+                              or non-characteristic words etc."),
+
+                    BlogPost("NinthTitle", uuid.UUID('25447284-aa74-4fb6-b7a0-2bb955f2b2b1')\
+                              ,"There are many variations of passages of Lorem Ipsum available,\
+                              but the majority have suffered alteration in some form, by injected\
+                              humour, or randomised words which don't look even slightly believable. \
+                              If you are going to use a passage of Lorem Ipsum, you need to\
+                              be sure there isn't anything embarrassing hidden in the middle\
+                              of text. All the Lorem Ipsum generators on the Internet tend to\
+                              repeat predefined chunks as necessary, making this the first \
+                              true generator on the Internet. It uses a dictionary of over 200\
+                              Latin words, combined with a handful of model sentence structures,\
+                              to generate Lorem Ipsum which looks reasonable. The generated Lorem \
+                              Ipsum is therefore always free from repetition, injected humour, \
+                              or non-characteristic words etc."),
+
+                    BlogPost("TenthTitle", uuid.UUID('99ae0e65-372b-4f4a-be88-776d6a4d92bd')\
+                              ,"There are many variations of passages of Lorem Ipsum available,\
+                              but the majority have suffered alteration in some form, by injected\
+                              humour, or randomised words which don't look even slightly believable. \
+                              If you are going to use a passage of Lorem Ipsum, you need to\
+                              be sure there isn't anything embarrassing hidden in the middle\
+                              of text. All the Lorem Ipsum generators on the Internet tend to\
+                              repeat predefined chunks as necessary, making this the first \
+                              true generator on the Internet. It uses a dictionary of over 200\
+                              Latin words, combined with a handful of model sentence structures,\
+                              to generate Lorem Ipsum which looks reasonable. The generated Lorem \
+                              Ipsum is therefore always free from repetition, injected humour, \
+                              or non-characteristic words etc."),
+
+                    BlogPost("EleventhTitle", uuid.UUID('99ae0e65-372b-4f4a-be88-776d6a4d92bd')\
+                              ,"There are many variations of passages of Lorem Ipsum available,\
+                              but the majority have suffered alteration in some form, by injected\
+                              humour, or randomised words which don't look even slightly believable. \
+                              If you are going to use a passage of Lorem Ipsum, you need to\
+                              be sure there isn't anything embarrassing hidden in the middle\
+                              of text. All the Lorem Ipsum generators on the Internet tend to\
+                              repeat predefined chunks as necessary, making this the first \
+                              true generator on the Internet. It uses a dictionary of over 200\
+                              Latin words, combined with a handful of model sentence structures,\
+                              to generate Lorem Ipsum which looks reasonable. The generated Lorem \
+                              Ipsum is therefore always free from repetition, injected humour, \
                               or non-characteristic words etc.")]
 
 
@@ -96,6 +166,16 @@ in_memory_posts[4].post_id = uuid.UUID('0d816f70-0ed1-4cee-b156-112462e6ea52')
 in_memory_posts[4].stamp.creation_time = '2012-May-02 20:12:40'
 in_memory_posts[5].post_id = uuid.UUID('c98026b6-08b2-45c9-981b-a6159124189d')
 in_memory_posts[5].stamp.creation_time = '2008-June-01 18:11:12'
+in_memory_posts[6].post_id = uuid.UUID('2bb62474-43fb-4643-b38e-a333f3999254')
+in_memory_posts[6].stamp.creation_time = '2004-July-02 19:10:11'
+in_memory_posts[7].post_id = uuid.UUID('3cb862a3-3bf7-44a2-83d8-7b7440588b68')
+in_memory_posts[7].stamp.creation_time = '2010-April-05 10:11:12'
+in_memory_posts[8].post_id = uuid.UUID('f4968b20-92fc-4ef6-a9f7-e84bafec6b9e')
+in_memory_posts[8].stamp.creation_time = '2000-January-01 08:12:11'
+in_memory_posts[9].post_id = uuid.UUID('1e260899-2bfb-4fc3-8175-17ecaf7c68e3')
+in_memory_posts[9].stamp.creation_time = '1992-May-05 19:01:11'
+in_memory_posts[10].post_id = uuid.UUID('be3e1383-d296-4956-85d2-d0da74c78531')
+in_memory_posts[10].stamp.creation_time = '1991-October-03 11:13:00'
 
 in_memory_users = [
     User('admin', 'admin@admin.com', 'adminpass'),

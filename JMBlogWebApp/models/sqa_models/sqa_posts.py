ff96fa38a9a3d935a715ee197dae835bac00f77b
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-13 16:01:00 +0200

    Moved models to separate folder and created internal module

--- JMBlogWebApp/models/sqa_models/sqa_posts.py
+++ JMBlogWebApp/models/sqa_models/sqa_posts.py
@@ -1,12 +1,13 @@
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
 class Posts(Base):
     __tablename__= 'posts'
-    posts_id = Column(UUID)
+    posts_id = Column(UUID, primary_key=True)
     creation_date = Column(DateTime)
     edit_date = Column(DateTime)
     author = Column(UUID)
--- JMBlogWebApp/models/sqa_models/sqa_users.py
+++ JMBlogWebApp/models/sqa_models/sqa_users.py
@@ -1,12 +1,14 @@
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.sqa_models.sqa_posts import Posts
 
 Base = declarative_base()
 
 class Users(Base):
     __tablename__ = 'users'
-    user_id = Column(UUID)
+    user_id = Column(UUID, primary_key=True)
     user_name = Column(String)
     user_email = Column(String)
     user_password = Column(String)

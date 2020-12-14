a3edc58f5bd83bcfaf1b52e37e6f21979a091310
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-14 04:02:21 +0200

    Implemented ORM for all Users methods

--- JMBlogWebApp/repository/users_db_repository.py
+++ JMBlogWebApp/repository/users_db_repository.py
@@ -11,38 +11,36 @@ class UsersDBRepository(UsersRepository):
             yield element
 
     def add_user(self, user):
-        self._db.create_connection()
-        self._db.execute('INSERT INTO users\
-                         (user_id,\
-                          user_name,\
-                          user_email,\
-                          user_password,\
-                          user_created_at,\
-                          user_modified_at)\
-                          VALUES(%s, %s, %s, %s, %s, %s)',
-                         (str(user.user_id),
-                          user.user_name,
-                          user.user_email,
-                          user.user_password,
-                          user.user_timestamp.creation_time,
-                          user.user_timestamp.edit_time))
+        self._db.start_session()
 
-        self._db.close_connection()
+        session = self._db.get_session()
+
+        user_to_add = Users()
+
+        user_to_add.user_id = (str(user.user_id))
+        user_to_add.user_name = user.user_name
+        user_to_add.user_email = user.user_email
+        user_to_add.user_password = user.user_password
+        user_to_add.user_created_at = user.user_timestamp.creation_time
+        user_to_add.user_modified_at = user.user_timestamp.edit_time
+
+        session.add(user_to_add)
+
+        self._db.close_session()
 
     def update_user(self, user):
-        self._db.create_connection()
-        self._db.execute("UPDATE users SET\
-        user_name = %s,\
-        user_email = %s,\
-        user_password = %s,\
-        user_created_at = %s,\
-        user_modified_at = %s WHERE user_id = %s",
-                         (user.user_name,
-                          user.user_email,
-                          user.user_password,
-                          user.user_timestamp.creation_time,
-                          user.user_timestamp.edit_time,
-                          user.user_id))
+        self._db.start_session()
+
+        session = self._db.get_session()
+
+        session.query(Users).filter(Users.user_id == user.user_id).update({
+            Users.user_name: user.user_name,
+            Users.user_email: user.user_email,
+            Users.user_password: user.user_password,
+            Users.user_created_at: user.user_timestamp.creation_time,
+            Users.user_modified_at: user.user_timestamp.edit_time})
+
+        self._db.close_session()
 
     def get_users(self):
         all_elements = []
@@ -51,57 +49,74 @@ class UsersDBRepository(UsersRepository):
         session = self._db.get_session()
 
         for user in session.query(Users).all():
-            current_user = User(user.user_name, user.user_email, user.user_password)
+            current_user = User(user.user_name,
+                                user.user_email,
+                                user.user_password)
+
             current_user.user_id = user.user_id
             current_user.user_timestamp.creation_time = user.user_created_at
             current_user.user_timestamp.edit_time = user.user_modified_at
+
             all_elements.append(current_user)
 
         self._db.close_session()
         return all_elements
 
     def get_user_by_id(self, user_id):
-        self._db.create_connection()
-        query_result = self._db.execute('SELECT * FROM users where user_id=%s', (str(user_id),))
+        self._db.start_session()
 
-        query_output = query_result.fetchone()
+        session = self._db.get_session()
 
-        user = User(
-            query_output[1],
-            query_output[2],
-            query_output[3])
-
-        user.user_id = query_output[0]
-        user.user_timestamp.creation_time = query_output[4]
-        user.user_timestamp.edit_time = query_output[5]
-
-        self._db.close_connection()
-        return user
-
-    def get_by_name(self, username):
-        self._db.create_connection()
-        query_result = self._db.execute("SELECT * FROM users WHERE user_name =%s", (username, ))
-
-        query_output = query_result.fetchone()
+        query_output = (session.query(Users)
+                        .filter(Users.user_id == user_id.hex)
+                        .first())
 
         if query_output is not None:
-            user = User(
-                query_output[1],
-                query_output[2],
-                query_output[3])
+            user = User(query_output.user_name,
+                        query_output.user_email,
+                        query_output.user_password)
 
-            user.user_password = query_output[3]
-            user.user_id = query_output[0]
-            user.user_timestamp.creation_time = query_output[4]
-            user.user_timestamp.edit_time = query_output[5]
+            user.user_password = query_output.user_password
+            user.user_id = query_output.user_id
+            user.user_timestamp.creation_time = query_output.user_created_at
+            user.user_timestamp.edit_time = query_output.user_modified_at
 
-        else:
-            user = None
+            self._db.close_session()
 
-        self._db.close_connection()
-        return user
+            return user
+        return None
+
+    def get_by_name(self, username):
+        self._db.start_session()
+
+        session = self._db.get_session()
+
+        query_output = (session.query(Users)
+                        .filter(Users.user_name == username)
+                        .first())
+
+        if query_output is not None:
+            user = User(query_output.user_name,
+                        query_output.user_email,
+                        query_output.user_password)
+
+            user.user_password = query_output.user_password
+            user.user_id = query_output.user_id
+            user.user_timestamp.creation_time = query_output.user_created_at
+            user.user_timestamp.edit_time = query_output.user_modified_at
+
+            self._db.close_session()
+
+            return user
+        return None
 
     def remove_user(self, user_id):
-        self._db.create_connection()
-        self._db.execute("DELETE FROM users WHERE user_id=%s;", (str(user_id),))
-        self._db.close_connection()
+        self._db.start_session()
+
+        session = self._db.get_session()
+
+        (session.query(Users)
+         .filter(Users.user_id == user_id.hex)
+         .delete(synchronize_session=False))
+
+        self._db.close_session()

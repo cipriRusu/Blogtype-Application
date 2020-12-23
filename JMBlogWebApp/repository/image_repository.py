d8cc590a2b68cd658928c50096accc52239b15a7
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-23 16:19:50 +0200

    Added base class for ImageRepository

---
+++ JMBlogWebApp/repository/image_repository.py
@@ -0,0 +1,24 @@
+import abc
+from abc import ABCMeta
+
+class ImageRepository(metaclass=ABCMeta):
+
+    @classmethod
+    def __subclasshook__(cls, sub):
+        if cls is ImageRepository:
+            if any('__iter__' in Q.__dict__ for Q in sub.__mro__):
+                return True
+            return False
+        return NotImplemented
+
+    @abc.abstractmethod
+    def add_image(self, added_image):
+        pass
+
+    @abc.abstractmethod
+    def remove_image(self, blog_post):
+        pass
+
+    @abc.abstractmethod
+    def get_image(self, blog_post):
+        pass

d4cf8aa424d05407fae6036aeb926da0859d7efe
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-23 16:20:23 +0200

    Added database image repository

---
+++ JMBlogWebApp/repository/image_db_repository.py
@@ -0,0 +1,21 @@
+﻿import os
+from flask import url_for
+from repository.image_repository import ImageRepository
+
+class ImageDbRepository(ImageRepository):
+    def __init__(self):
+        pass
+
+    def add_image(self, added_image):
+        added_image.save(os.path.join('static/images', added_image.filename))
+
+    def remove_image(self, blog_post):
+        if os.path.exists(os.path.join('static', blog_post.img_path)):
+            os.remove(os.path.join('static', blog_post.img_path))
+        blog_post.img_path = None
+
+    def get_image(self, blog_post):
+        if (blog_post.img_path is None or not
+                os.path.exists(os.path.join('static', blog_post.img_path))):
+            return url_for('static', filename='images/default.png')
+        return url_for('static', filename=blog_post.img_path)

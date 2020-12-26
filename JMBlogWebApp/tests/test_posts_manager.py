67d53a83aa0649a23b9e8a287c26f1e9dddf15e7
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-26 23:55:45 +0200

    Added unit tests for image upload,removal,flashing(corner cases)

--- JMBlogWebApp/tests/test_posts_manager.py
+++ JMBlogWebApp/tests/test_posts_manager.py
@@ -1,3 +1,7 @@
+import io
+import base64
+from repository.in_memory_data import in_memory_photos
+
 def test_root_redirects(configured_app):
     response = configured_app.get('/').status_code
     assert response == 302
@@ -234,3 +238,119 @@ def test_pagination_dissapears_if_no_further_posts_are_available(configured_app)
     post_page = configured_app.get('/posts/?Users=FirstAuthor', follow_redirects=True).data
 
     assert b'Next' not in post_page and b'Previous' not in post_page
+
+
+def test_post_addition_adds_no_image_if_none_is_selected(configured_app):
+    login_data = {"NameInput": "FirstAuthor", "PasswordInput": "fpass"}
+
+    configured_app.post('/authentication/login',
+                         data=login_data,
+                         follow_redirects=True)
+
+    added_post = {"NameInput":"TestTitle", "ContentInput":"TestContent"}
+
+    response_data = configured_app.post('/posts/add', 
+                                         data=added_post, 
+                                         follow_redirects=True).data
+
+    assert b'/images/default.png' in response_data
+
+def test_post_loads_image_if_image_exists(configured_app):
+    login_data = {"NameInput": "FirstAuthor", "PasswordInput": "fpass"}
+
+    configured_app.post('/authentication/login', data=login_data, follow_redirects=True)
+
+    response_data = configured_app.get('/posts/a656f973-5b82-462d-aff7-8d2c6c3e4fa2', 
+                                        follow_redirects=True).data
+
+    assert in_memory_photos['local3'].encode('utf-8') in response_data
+
+def test_post_loads_default_image_if_image_is_removed(configured_app):
+    login_data = {"NameInput": "SecondAuthor", "PasswordInput": "spass"}
+
+    configured_app.post('/authentication/login', data=login_data, follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/daca57d1-c180-4e0a-8394-f5c95a5d5f23', 
+                                        data = {'remove-image': ''},
+                                        follow_redirects=True).data
+
+    assert b'/images/default.png' in response_data
+
+def test_post_changes_image_when_new_image_is_loaded(configured_app):
+    login_data = {"NameInput": "ThirdAuthor", "PasswordInput": "tpass"}
+
+    post_data = {'update-image': '','image-file': ''}
+
+    configured_app.post('/authentication/login', data=login_data, follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/be3e1383-d296-4956-85d2-d0da74c78531',
+                                        data=post_data,
+                                        follow_redirects=True).data
+
+    assert b'/images/default.png' in response_data
+
+def test_post_default_image_is_replaced_if_new_image_is_loaded(configured_app):
+    login_data = {"NameInput": "FirstAuthor", "PasswordInput": "fpass"}
+
+    post_data = {'update-image': '',
+                 'image-file': (io.BytesIO(b'test_image_content'), "test.jpg")}
+
+    configured_app.post('/authentication/login', data=login_data, follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/3cb862a3-3bf7-44a2-83d8-7b7440588b68',
+                                        data=post_data,
+                                        follow_redirects=True).data
+
+    encoded_image_data = base64.b64encode(b'test_image_content')
+
+    assert encoded_image_data in response_data
+
+def test_post_flash_message_for_no_selected_image(configured_app):
+    login_data = {"NameInput": "admin", 
+                  "PasswordInput": "adminpass"}
+
+    post_data = {'update-image': '',
+                 'image-file': (io.BytesIO(b'test_image_content'), '')}
+
+    configured_app.post('/authentication/login',
+                        data=login_data,
+                        follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/0d816f70-0ed1-4cee-b156-112462e6ea52',
+                                        data=post_data,
+                                        follow_redirects=True).data
+
+    assert b'No image found. Provide a valid file' in response_data
+
+def test_post_flash_message_for_illegal_format_selected_as_image(configured_app):
+    login_data = {"NameInput": "admin", 
+                  "PasswordInput": "adminpass"}
+
+    post_data = {'update-image': '',
+                 'image-file': (io.BytesIO(b'test_illegal_content'), 'illegal.exe')}
+
+    configured_app.post('/authentication/login',
+                        data=login_data,
+                        follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/0d816f70-0ed1-4cee-b156-112462e6ea52',
+                                        data=post_data,
+                                        follow_redirects=True).data
+
+    assert b'Invalid file type! Make sure a valid file format is selected' in response_data
+
+def test_post_flash_message_for_default_image_to_remove(configured_app):
+    login_data = {"NameInput": "admin", 
+                  "PasswordInput": "adminpass"}
+
+    post_data = {'remove-image': ''}
+
+    configured_app.post('/authentication/login',
+                        data=login_data,
+                        follow_redirects=True)
+
+    response_data = configured_app.post('/posts/update/0d816f70-0ed1-4cee-b156-112462e6ea52',
+                                        data=post_data,
+                                        follow_redirects=True).data
+
+    assert b'No image present. Nothing to remove' in response_data

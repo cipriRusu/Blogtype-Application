b968081df7d53643f0bcdf22788eae8a23a008fd
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-27 17:09:51 +0200

    Added base route scaffold for user statistics

---
+++ JMBlogWebApp/views/statistics_manager.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
+from setup import services_listing as services
+from views.decorators import setup_decorators
+from views.decorators import inject_decorators
+
+statistics_manager = Blueprint('statistics_manager',
+                               __name__,
+                               url_prefix='/user_statistics', 
+                               template_folder='templates')
+
+@login_manager.route('/', methods=["GET"])
+@setup_decorators.config_check
+@inject_decorators.inject
+def user_statistics():
+    print('test')

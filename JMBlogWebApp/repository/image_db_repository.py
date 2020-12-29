9ca19746d631dec51b0225aa8bb1215d57cdc50d
Cipri Rusu <cipri91rusu@gmail.com>
2020-12-29 12:07:26 +0200

    Bugfix -> managed situation when image filename is identical

--- JMBlogWebApp/repository/image_db_repository.py
+++ JMBlogWebApp/repository/image_db_repository.py
@@ -7,6 +7,7 @@ from repository.image_repository import ImageRepository
 LEGAL_EXTENSIONS = ['.jpg', '.jpeg', '.png']
 
 class ImageDbRepository(ImageRepository):
+    COUNTER = 0
     def __init__(self):
         pass
 
@@ -17,7 +18,14 @@ class ImageDbRepository(ImageRepository):
         if added_image.filename[-4:].lower() not in LEGAL_EXTENSIONS:
             raise FileFormatException()
 
-        added_image.save(os.path.join('static/images', added_image.filename))
+        source_path = os.path.join('static/images', added_image.filename)
+
+        if os.path.exists(source_path):
+            alternative_filename = str(ImageDbRepository.COUNTER) + added_image.filename
+            source_path = os.path.join('static/images', alternative_filename)
+            added_image.filename = alternative_filename
+            ImageDbRepository.COUNTER = ImageDbRepository.COUNTER + 1
+        added_image.save(source_path)
         blog_post.img_path = 'images/{}'.format(added_image.filename)
 
     def remove_image(self, blog_post):

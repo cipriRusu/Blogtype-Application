24e8c1e0d1844dcb5246525534c7ae67c4c2b4fa
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-04 16:29:07 +0200

    Appended final semicolon and added picture path returning script (server side)

--- JMBlogWebApp/static/scripts/post_script.js
+++ JMBlogWebApp/static/scripts/post_script.js
@@ -4,31 +4,38 @@ var path = '/api' + window.location.pathname
 function loadData(path) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', path, true)
+    xhr.open('GET', path, true);
 
     xhr.onload = function() {
         if (this.status == 200) {
-            post_data = JSON.parse(this.responseText)
+            post_data = JSON.parse(this.responseText);
 
-            var title = document.getElementById('Article_Title')
-            title.appendChild(document.createTextNode(post_data.title))
+            var title = document.getElementById('Article_Title');
+            title.appendChild(document.createTextNode(post_data.title));
 
-            var author = document.getElementById('Article_Author')
-            author.appendChild(document.createTextNode(post_data.author))
+            var author = document.getElementById('Article_Author');
+            author.appendChild(document.createTextNode(post_data.author));
 
-            var create_datetime = document.getElementById('Created_Datetime')
-            create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time))
+            var create_datetime = document.getElementById('Created_Datetime');
+            create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time));
 
-            var edit_datetime = document.getElementById('Edited_Datetime')
+            var edit_datetime = document.getElementById('Edited_Datetime');
 
             if (post_data.timestamp.edit_time == null) {
-                edit_datetime.appendChild(document.createTextNode('Never'))
+                edit_datetime.appendChild(document.createTextNode('Never'));
             } else {
-                edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time))
+                edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time));
             }
 
-            var post_content = document.getElementById('Article_Content')
-            post_content.appendChild(document.createTextNode(post_data.content))
+            var post_content = document.getElementById('Article_Content');
+            post_content.appendChild(document.createTextNode(post_data.content));
+
+            var image_content = document.getElementById('Article_Image');
+            if (post_data.image_path == null) {
+                image_content.setAttribute('src', '/images/default.png');
+            } else {
+                image_content.setAttribute('src', post_data.image_path);
+            }
         }
     }
 

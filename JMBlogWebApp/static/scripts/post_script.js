786487e2c6792e37348830a5dd079e0588c218c7
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-09 22:39:02 +0200

    switched to fetch api from xmlhttp

--- JMBlogWebApp/static/scripts/post_script.js
+++ JMBlogWebApp/static/scripts/post_script.js
@@ -2,44 +2,32 @@ var host = window.location.origin
 var path = '/api' + window.location.pathname
 
 function loadData(path) {
-    var xhr = new XMLHttpRequest();
+    fetch(path).then(
+        function (response) {
+            response.json().then(function (post_data) {
+                var title = document.getElementById('Article_Title');
+                title.appendChild(document.createTextNode(post_data.title));
 
-    xhr.open('GET', path, true);
+                var author = document.getElementById('Article_Author');
+                author.appendChild(document.createTextNode(post_data.author));
 
-    xhr.onload = function() {
-        if (this.status == 200) {
-            post_data = JSON.parse(this.responseText);
+                var create_datetime = document.getElementById('Created_Datetime');
+                create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time));
 
-            var title = document.getElementById('Article_Title');
-            title.appendChild(document.createTextNode(post_data.title));
+                var edit_datetime = document.getElementById('Edited_Datetime');
+                if (post_data.timestamp.creation_time == post_data.timestamp.edit_time) {
+                    edit_datetime.appendChild(document.createTextNode('Never'))
+                } else {
+                    edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time));
+                }
 
-            var author = document.getElementById('Article_Author');
-            author.appendChild(document.createTextNode(post_data.author));
+                var post_content = document.getElementById('Article_Content');
+                post_content.appendChild(document.createTextNode(post_data.content));
 
-            var create_datetime = document.getElementById('Created_Datetime');
-            create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time));
-
-            var edit_datetime = document.getElementById('Edited_Datetime');
-
-            if (post_data.timestamp.edit_time == null) {
-                edit_datetime.appendChild(document.createTextNode('Never'));
-            } else {
-                edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time));
-            }
-
-            var post_content = document.getElementById('Article_Content');
-            post_content.appendChild(document.createTextNode(post_data.content));
-
-            var image_content = document.getElementById('Article_Image');
-            if (post_data.image_path == null) {
-                image_content.setAttribute('src', '/images/default.png');
-            } else {
+                var image_content = document.getElementById('Article_Image');
                 image_content.setAttribute('src', post_data.image_path);
-            }
-        }
-    }
-
-    xhr.send();
+            });
+        });
 }
 
 loadData(host + path);

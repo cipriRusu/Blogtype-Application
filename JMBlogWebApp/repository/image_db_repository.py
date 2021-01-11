d24035dd2dc7c3d7aa65c6610bcb089e5fcc5757
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-11 09:05:56 +0200

    removed file saving logic from repository (moved into image_service)

--- JMBlogWebApp/repository/image_db_repository.py
+++ JMBlogWebApp/repository/image_db_repository.py
@@ -11,34 +11,20 @@ class ImageDbRepository(ImageRepository):
     def __init__(self):
         pass
 
-    def add_image(self, blog_post, added_image):
-        if added_image.filename == '':
-            raise FilePathException()
-
-        if added_image.filename[-4:].lower() not in LEGAL_EXTENSIONS:
-            raise FileFormatException()
-
-        source_path = os.path.join('static/images', str(uuid.uuid4())[:4] + '_' + added_image.filename)
-
-        added_image.save(source_path)
-        
-        blog_post.update(img_path='/images/{}'.format(added_image.filename))
+    def add_image(self, blog_post):
+        pass
 
     def update_image(self, blog_post):
         pass
 
     def remove_image(self, blog_post):
-        if blog_post.img_path is None:
-            raise FilePathException()
-
-        if not os.path.exists(os.path.join('static', blog_post.img_path)):
-            raise FilePathException()
 
         os.remove(os.path.join('static', blog_post.img_path))
+
         blog_post.update(img_path=None)
 
     def get_image(self, blog_post):
         if (blog_post.img_path is None or not
-                os.path.exists(os.path.join('static', blog_post.img_path))):
+                os.path.exists(os.path.join('static/images/' + blog_post.img_path))):
             return url_for('static', filename='images/default.png')
-        return url_for('static', filename=blog_post.img_path)
+        return url_for('static', filename='images/' + blog_post.img_path)

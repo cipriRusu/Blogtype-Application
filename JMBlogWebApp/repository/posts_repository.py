3764517852654b647e74e45cdd561686fd9cc007
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-14 13:14:44 +0200

    renamed blog_post instance in update method

--- JMBlogWebApp/repository/posts_repository.py
+++ JMBlogWebApp/repository/posts_repository.py
@@ -16,7 +16,7 @@ class PostsRepository(metaclass=ABCMeta):
         pass
 
     @abc.abstractmethod
-    def update_post(self, item, remove_image=False):
+    def update_post(self, blog_post, remove_image=False):
         pass
 
     @abc.abstractmethod

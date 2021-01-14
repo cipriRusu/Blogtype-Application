3b8b4820078fc72bca5f2971922de7e23471f079
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-14 20:04:21 +0200

    Handled image adding to post (image repository)

--- JMBlogWebApp/repository/image_db_repository.py
+++ JMBlogWebApp/repository/image_db_repository.py
@@ -1,5 +1,6 @@
 ﻿import os
-from flask import url_for
+import uuid
+from flask import url_for, flash
 from repository.image_repository import ImageRepository
 
 LEGAL_EXTENSIONS = ['.jpg', '.jpeg', '.png']
@@ -9,15 +10,18 @@ class ImageDbRepository(ImageRepository):
         pass
 
     def add_image(self, blog_post):
-        pass
+        if blog_post.img_path.filename == '':
+            return None
+        current_file = str(uuid.uuid4())[:4] + '_' + blog_post.img_path.filename
+        source_path = os.path.join('static/images/', current_file)
+        blog_post.img_path.save(source_path)
+        return current_file
 
     def update_image(self, old_blogpost, blog_post, remove_image=False):
         pass
 
     def remove_image(self, blog_post):
-
         os.remove(os.path.join('static', blog_post.img_path))
-
         blog_post.update(img_path=None)
 
     def get_image(self, blog_post):

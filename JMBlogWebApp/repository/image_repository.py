ac890797efa37e3659e9437129fc4af9dbdcae3b
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-16 14:09:51 +0200

    removed unused parameter

--- JMBlogWebApp/repository/image_repository.py
+++ JMBlogWebApp/repository/image_repository.py
@@ -16,7 +16,7 @@ class ImageRepository(metaclass=ABCMeta):
         pass
 
     @abc.abstractmethod
-    def update_image(self, old_blogpost, blog_post, remove_image=False):
+    def update_image(self, blog_post, remove_image=False):
         pass
 
     @abc.abstractmethod

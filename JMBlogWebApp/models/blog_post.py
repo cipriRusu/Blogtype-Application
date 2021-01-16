26d9966d37afb84e4e0600f5bb3b83ea9f827bd2
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-16 14:03:55 +0200

    Added separate property for uploaded file

--- JMBlogWebApp/models/blog_post.py
+++ JMBlogWebApp/models/blog_post.py
@@ -10,16 +10,19 @@ class BlogPost:
         self.content = content
         self.preview = content[0:30]
         self.img_path = img_path
+        self.uploaded_file = None
 
     def __eq__(self, other):
         return self.post_id == other.post_id
 
-    def update(self, title=None, content=None, img_path=None):
+    def update(self, title=None, content=None):
         self.title = title if title is not None else self.title
         self.content = content if content is not None else self.content
-        self.img_path = img_path
         self.preview = self.content[0:30]
         self.stamp.on_edit()
+
+    def upload(self, uploaded_file):
+        self.uploaded_file = uploaded_file
 
     def to_dict(self):
         return {'post_id': self.post_id,

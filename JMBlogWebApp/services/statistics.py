1e280a7384c1d0ad9e1595e9729818127c79d455
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-17 14:42:34 +0200

    Moved statistics logic into method

--- JMBlogWebApp/services/statistics.py
+++ JMBlogWebApp/services/statistics.py
@@ -1,12 +1,27 @@
-from models.user_statistics import UserStatistics
-
 class Statistics():
     def __init__(self, posts_repository):
         self._posts = posts_repository
-        self._stats = UserStatistics()
 
     def get_statistics(self, selected_user):
         username = None if selected_user in (None, 'All Users') else selected_user
-        self._stats.set_name(username)
-        self._stats.set_posts(self._posts.get_all(username))
-        return self._stats
+
+        total_stats = {}
+
+        for post in self._posts.get_all(username):
+            total_stats.setdefault(post.stamp.creation_time.year, [])
+            (total_stats[post.stamp.creation_time.year]
+             .append(post
+                     .stamp
+                     .creation_time
+                     .strftime('%B')))
+
+        for year, yearly_data in total_stats.items():
+            total_stats[year] = None
+            monthly_data = {}
+            for month in yearly_data:
+                if month in monthly_data:
+                    monthly_data[month] += 1
+                else:
+                    monthly_data[month] = 1
+            total_stats[year] = monthly_data
+        return total_stats

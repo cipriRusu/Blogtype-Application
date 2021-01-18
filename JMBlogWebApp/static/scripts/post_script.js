e7ca370764839401b987f4e682d895f97b31db32
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-18 09:52:04 +0200

    handled error catching in fetchapi

--- JMBlogWebApp/static/scripts/post_script.js
+++ JMBlogWebApp/static/scripts/post_script.js
@@ -26,6 +26,25 @@ function loadData(path) {
 
                 var image_content = document.getElementById('Article_Image');
                 image_content.setAttribute('src', post_data.image_path);
+            }).catch((err) => {
+                var title = document.getElementById('Article_Title');
+                title.appendChild(document.createTextNode("Not available"));
+
+                var author = document.getElementById('Article_Author');
+                author.appendChild(document.createTextNode("Not available"));
+
+                var create_datetime = document.getElementById('Created_Datetime');
+                create_datetime.appendChild(document.createTextNode("Not available"));
+
+                var edit_datetime = document.getElementById('Edited_Datetime');
+                if (post_data.timestamp.creation_time == post_data.timestamp.edit_time) {
+                    edit_datetime.appendChild(document.createTextNode('Never'))
+                } else {
+                    edit_datetime.appendChild(document.createTextNode("Not available"));
+                }
+
+                var post_content = document.getElementById('Article_Content');
+                post_content.appendChild(document.createTextNode("Not available"));
             });
         });
 }

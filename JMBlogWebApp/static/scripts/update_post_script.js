e73f4144f5a1711cd68df69da3ff4e757371eb4f
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-18 15:39:45 +0200

    Added scripts to prevent foreign clicks and multiple clicks (click/request)

--- JMBlogWebApp/static/scripts/update_post_script.js
+++ JMBlogWebApp/static/scripts/update_post_script.js
@@ -12,3 +12,17 @@ function loadData(path) {
 }
 
 loadData(host + path);
+
+function submitLimit() {
+    update_button = document.getElementById('picture-update');
+    setTimeout(() => { update_button.setAttribute('disabled', 'disabled') }, 1);
+    setTimeout(() => { update_button.removeAttribute('disabled') }, 4000);
+
+    remove_button = document.getElementById('picture-remove');
+    setTimeout(() => { remove_button.setAttribute('disabled', 'disabled') }, 1);
+    setTimeout(() => { remove_button.removeAttribute('disabled') }, 4000);
+
+    submit_button = document.getElementById('submit-form');
+    setTimeout(() => { submit_button.setAttribute('disabled', 'disabled') }, 1);
+    setTimeout(() => { submit_button.removeAttribute('disabled') }, 4000);
+}

80d5976b2f7776a092134869c39b0b586e74a318
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-21 00:28:13 +0200

    Removed post path retrieving from js (handled into template)

--- JMBlogWebApp/static/scripts/post_script.js
+++ JMBlogWebApp/static/scripts/post_script.js
@@ -1,52 +1,27 @@
-var host = window.location.origin
-var path = '/api' + window.location.pathname
+function getPostData(path) {
+    fetch(path).then(function (response) {
+        response.json().then(function (post_data) {
+            var title = document.getElementById('Article_Title');
+            title.appendChild(document.createTextNode(post_data.title));
 
-function loadData(path) {
-    fetch(path).then(
-        function (response) {
-            response.json().then(function (post_data) {
-                var title = document.getElementById('Article_Title');
-                title.appendChild(document.createTextNode(post_data.title));
+            var author = document.getElementById('Article_Author');
+            author.appendChild(document.createTextNode(post_data.author));
 
-                var author = document.getElementById('Article_Author');
-                author.appendChild(document.createTextNode(post_data.author));
+            var create_datetime = document.getElementById('Created_Datetime');
+            create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time));
 
-                var create_datetime = document.getElementById('Created_Datetime');
-                create_datetime.appendChild(document.createTextNode(post_data.timestamp.creation_time));
+            var edit_datetime = document.getElementById('Edited_Datetime');
+            if (post_data.timestamp.creation_time == post_data.timestamp.edit_time) {
+                edit_datetime.appendChild(document.createTextNode('Never'))
+            } else {
+                edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time));
+            }
 
-                var edit_datetime = document.getElementById('Edited_Datetime');
-                if (post_data.timestamp.creation_time == post_data.timestamp.edit_time) {
-                    edit_datetime.appendChild(document.createTextNode('Never'))
-                } else {
-                    edit_datetime.appendChild(document.createTextNode(post_data.timestamp.edit_time));
-                }
+            var post_content = document.getElementById('Article_Content');
+            post_content.appendChild(document.createTextNode(post_data.content));
 
-                var post_content = document.getElementById('Article_Content');
-                post_content.appendChild(document.createTextNode(post_data.content));
-
-                var image_content = document.getElementById('Article_Image');
-                image_content.setAttribute('src', post_data.image_path);
-            }).catch((err) => {
-                var title = document.getElementById('Article_Title');
-                title.appendChild(document.createTextNode("Not available"));
-
-                var author = document.getElementById('Article_Author');
-                author.appendChild(document.createTextNode("Not available"));
-
-                var create_datetime = document.getElementById('Created_Datetime');
-                create_datetime.appendChild(document.createTextNode("Not available"));
-
-                var edit_datetime = document.getElementById('Edited_Datetime');
-                if (post_data.timestamp.creation_time == post_data.timestamp.edit_time) {
-                    edit_datetime.appendChild(document.createTextNode('Never'))
-                } else {
-                    edit_datetime.appendChild(document.createTextNode("Not available"));
-                }
-
-                var post_content = document.getElementById('Article_Content');
-                post_content.appendChild(document.createTextNode("Not available"));
-            });
+            var image_content = document.getElementById('Article_Image');
+            image_content.setAttribute('src', post_data.image_path);
         });
+    });
 }
-
-loadData(host + path);

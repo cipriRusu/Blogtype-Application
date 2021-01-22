a428c3b9223ac0796e5a4fd9009da9056fa71fb0
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-22 16:55:03 +0200

    renamed api tests to api_manager_posts (kept only post api testing)

--- JMBlogWebApp/tests/test_api_manager_posts.py
+++ JMBlogWebApp/tests/test_api_manager_posts.py
@@ -1,20 +1,6 @@
 import base64
 import json
 
-def test_get_valid_code_for_valid_login(configured_app):
-    credentials = base64.b64encode(b"admin:adminpass").decode('utf-8')
-    login_token_code = (configured_app.post('/api/authentication/login',
-                                            headers={"authorization": "Basic " + credentials})
-                        .status_code)
-    assert login_token_code == 200
-
-def test_get_valid_code_for_invalid_login(configured_app):
-    credentials = base64.b64encode(b"invalid:invalid").decode('utf-8')
-    login_token_code = (configured_app.post('/api/authentication/login',
-                                            headers={"authorization": "Basic " + credentials})
-                        .status_code)
-    assert login_token_code == 403
-
 def test_get_all_posts_route(configured_app):
     response = configured_app.get('/api/posts', follow_redirects=True).data
 

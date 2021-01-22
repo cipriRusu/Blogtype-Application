0487a6723c34f5c27f22582c10d49d1ca82112ca
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-22 16:43:14 +0200

    renamed api manager to api manager posts  (removed authentication routes from blueprint, moved to separate blueprint)

--- JMBlogWebApp/views/api_manager_posts.py
+++ JMBlogWebApp/views/api_manager_posts.py
@@ -1,15 +1,15 @@
 from exceptions.post_not_found_exception import PostNotFoundException
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from views.decorators import setup_decorators
 from views.decorators import inject_decorators
 from views.decorators import token_decorators
 from setup import services_listing as services
 
-api_manager = Blueprint('api_manager', __name__, url_prefix='/api',
-                        template_folder='templates',
-                        static_folder='static')
+api_manager_posts = Blueprint('api_manager_posts', __name__, url_prefix='/api/posts/',
+                              template_folder='templates',
+                              static_folder='static')
 
-@api_manager.route('/posts/', methods=["GET"])
+@api_manager_posts.route('/', methods=["GET"])
 @setup_decorators.config_check
 @inject_decorators.inject
 def get_all(posts: services.DATA_SOURCE_POSTS):
@@ -18,7 +18,7 @@ def get_all(posts: services.DATA_SOURCE_POSTS):
         all_json_posts.append(post.to_dict())
     return jsonify(results=all_json_posts)
 
-@api_manager.route('/posts/<uuid:post_index>', methods=["GET"])
+@api_manager_posts.route('/<uuid:post_index>', methods=["GET"])
 @setup_decorators.config_check
 @inject_decorators.inject
 def get_item(post_index,
@@ -29,25 +29,10 @@ def get_item(post_index,
         return jsonify({"message": "Resource not available"}), 404
     return jsonify(post.to_dict())
 
-@api_manager.route('/posts/remove/<uuid:post_index>')
+@api_manager_posts.route('/remove/<uuid:post_index>')
 @setup_decorators.config_check
 @inject_decorators.inject
 @token_decorators.token_required
 def remove_item(post_index, posts: services.DATA_SOURCE_POSTS):
     posts.remove(post_index)
     return jsonify({'message' : 'Post was deleted successfully!'})
-
-@api_manager.route('/login', methods=["POST"])
-@setup_decorators.config_check
-@inject_decorators.inject
-def login(auth_manager: services.USER_LOGIN,
-          users: services.DATA_SOURCE_USERS,
-          token_manager: services.TOKEN_HANDLING):
-
-    if auth_manager.user_login(request.authorization.username,
-                               request.authorization.password):
-
-        found_user = users.get_by_name(request.authorization.username)
-        generated_token = token_manager.create_token(found_user)
-        return jsonify({'token': generated_token.decode()})
-    return jsonify({'token' : 'authorization failed'}), 403

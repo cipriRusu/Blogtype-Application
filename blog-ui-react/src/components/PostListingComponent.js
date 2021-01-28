4153195ab0f886b210ed3c486535318b0ab97916
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-28 11:08:35 +0200

    Bugfix: handled continuous request sending every mount (post request flood)

--- blog-ui-react/src/components/PostListingComponent.js
+++ blog-ui-react/src/components/PostListingComponent.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
+import Media from 'react-bootstrap/Media'
+
 
 const PostListingComponent = () => {
     const [values, updateData] = React.useState([])
 
-    fetch('http://localhost:4449/api/posts/')
-        .then(response => response.json())
-        .then(data => updateData(data))
+    useEffect(() => {
+        let mounted = true;
+        fetch('http://localhost:4449/api/posts/')
+            .then(response => response.json())
+            .then(data => {
+                if (mounted) {
+                    updateData(data)
+                }
+            })
+        return () => mounted = false;
+    }, []
+    )
     return (
         <Accordion>{values.map((value, index) =>
             <Card key={index}>
@@ -16,11 +27,15 @@ const PostListingComponent = () => {
                     <p>Post title: {value.title}</p>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={index + 1}>
-                    <Card.Body>
-                        <p>Post author: {value.author}</p>
-                        <p>Post title: {value.title}</p>
-                        <p>Post preview: {value.preview}</p>
-                    </Card.Body>
+                    <div style={{ margin: "20px" }}>
+                        <Media>
+                            <Media.Body>
+                                <p>Post author: {value.author}</p>
+                                <p>Post title: {value.title}</p>
+                                <p>Post preview: {value.preview}</p>
+                            </Media.Body>
+                        </Media>
+                    </div>
                 </Accordion.Collapse>
             </Card>
         )}

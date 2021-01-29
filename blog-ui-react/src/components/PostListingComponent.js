d959b64d206a562a920c005f0194902e756f3ae9
Cipri Rusu <cipri91rusu@gmail.com>
2021-01-29 10:57:19 +0200

    Implemented image display in posts listing / Implemented post navigation (TODO: post page)

--- blog-ui-react/src/components/PostListingComponent.js
+++ blog-ui-react/src/components/PostListingComponent.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 import Media from 'react-bootstrap/Media'
-
+import Button from 'react-bootstrap/Button'
+import { useHistory } from "react-router-dom";
 
 const PostListingComponent = () => {
     const [values, updateData] = React.useState([])
+    const history = useHistory();
 
     useEffect(() => {
         let mounted = true;
@@ -19,6 +21,11 @@ const PostListingComponent = () => {
         return () => mounted = false;
     }, []
     )
+
+    function navigateToPost(params) {
+        history.push('/post/'.concat(params))
+    }
+
     return (
         <Accordion>{values.map((value, index) =>
             <Card key={index}>
@@ -34,7 +41,15 @@ const PostListingComponent = () => {
                                 <p>Post title: {value.title}</p>
                                 <p>Post preview: {value.preview}</p>
                             </Media.Body>
+                            <img
+                                width={160}
+                                height={100}
+                                className="mr-3"
+                                src={"http://localhost:4449" + value.image_path}
+                                alt="Generic placeholder"
+                            />
                         </Media>
+                        <Button type="button" outline onClick={e => navigateToPost(value.post_id)} variant="secondary" size="sm">Go To Post</Button>
                     </div>
                 </Accordion.Collapse>
             </Card>

5080df4201cf4d1f6e096c01df3d4d7deda2698c
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-01 10:43:00 +0200

    Added styling to post listing

--- blog-ui-react/src/components/PostListed.js
+++ blog-ui-react/src/components/PostListed.js
@@ -8,22 +8,22 @@ import { Link } from 'react-router-dom';
 const PostListed = (props) => {
     return (
     <Card>
-        <Accordion.Toggle as={Card.Header} eventKey={props.count + 1}>
-            <p>Post author: {props.post.author}</p>
-            <p>Post title: {props.post.title}</p>
+        <Accordion.Toggle as={Card.Header} eventKey={props.count + 1} style={{ padding: "0px", paddingLeft: "10px", paddingTop: "20px", paddingBottom: "20px"}}>
+            <p style={{ margin: "0px"}}>Post author: {props.post.author}</p>
+            <p style={{ margin: "0px"}}>Post title: {props.post.title}</p>
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey={props.count + 1}>
-                <div style={{ margin: "20px" }}>
+            <Accordion.Collapse eventKey={props.count + 1} style={{ paddingLeft: "10px"}}>
+                <div style={{ paddingTop: "20px", paddingBottom: "20px"}}>
                     <Media>
                         <Media.Body>
-                            <p>Post author: {props.post.author}</p>
-                            <p>Post title: {props.post.title}</p>
-                            <p>Post preview: {props.post.preview}</p>
+                            <p style={{ margin: "0px"}}>Post author: {props.post.author}</p>
+                            <p style={{ margin: "0px"}}>Post title: {props.post.title}</p>
+                            <p style={{ margin: "0px"}}>Post preview: {props.post.preview}</p>
                         </Media.Body>
                         <img
+                        style={{ marginRight: "20px"}}
                         width={160}
                         height={100}
-                        className="mr-3"
                         src={props.post.image_path}
                         alt="Generic placeholder"
                         />

5b29d2e08328a3504b351512b3c62cf27683d5fa
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-01 10:43:47 +0200

    Added styling to navigation bar

--- blog-ui-react/src/components/Navigation.js
+++ blog-ui-react/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavigationStyling.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-bootstrap';
@@ -7,10 +8,10 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <Navbar bg="dark" variant="dark" fixed="static-top">
-            <Nav.Link><Link to='/posts'>Posts</Link></Nav.Link>
-            <Nav.Link><Link to='/users'> Users</Link></Nav.Link>
-            <Nav.Link><Link to='/stats'> Statstics</Link></Nav.Link>
+        <Navbar style={{ padding: "0px", paddingTop: "20px", paddingBottom: "20px"}} bg="dark" variant="dark" fixed="static-top">
+            <Nav.Link style={{ padding: "0px", paddingRight: "50px", paddingLeft:"11px"}}><Link to='/posts'>Posts</Link></Nav.Link>
+            <Nav.Link style={{ padding: "0px", paddingRight: "50px"}}><Link to='/users'> Users</Link></Nav.Link>
+            <Nav.Link style={{ padding: "0px", paddingRight: "50px"}}><Link to='/stats'> Statstics</Link></Nav.Link>
         </Navbar>
     )
 }

5618e91d2aa1f46bdf3410b4e99b7da805835936
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-01 13:16:26 +0200

    removed inline sytling (temp)

--- blog-ui-react/src/components/Navigation.js
+++ blog-ui-react/src/components/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './NavigationStyling.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav';
 import { NavLink } from 'react-bootstrap';
@@ -8,10 +7,10 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <Navbar style={{ padding: "0px", paddingTop: "20px", paddingBottom: "20px"}} bg="dark" variant="dark" fixed="static-top">
-            <Nav.Link style={{ padding: "0px", paddingRight: "50px", paddingLeft:"11px"}}><Link to='/posts'>Posts</Link></Nav.Link>
-            <Nav.Link style={{ padding: "0px", paddingRight: "50px"}}><Link to='/users'> Users</Link></Nav.Link>
-            <Nav.Link style={{ padding: "0px", paddingRight: "50px"}}><Link to='/stats'> Statstics</Link></Nav.Link>
+        <Navbar className="Test" bg="dark" variant="dark" fixed="static-top">
+            <Nav.Link><Link to='/posts'>Posts</Link></Nav.Link>
+            <Nav.Link><Link to='/users'> Users</Link></Nav.Link>
+            <Nav.Link><Link to='/stats'> Statstics</Link></Nav.Link>
         </Navbar>
     )
 }

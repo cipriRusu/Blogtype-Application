ea581a8bcbdfb5981b045ceadec48711c0c8e677
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-01 10:43:27 +0200

    Added post styling

--- blog-ui-react/src/components/Post.js
+++ blog-ui-react/src/components/Post.js
@@ -4,9 +4,10 @@ import usePostsData from './usePostsData'
 const Post = ({ match }) => {
 	const post_data = usePostsData(`http://localhost:4449/api/posts/${match.params.id}`);
 	return (
-	<div style={{ margin: "20px", display: 'grid' }}>
+	<div style={{ display: 'grid', margin: '11px' }}>
 		<h3>Post Title: {post_data.title}</h3>
-		<h3>Post Author: {post_data.author}</h3>
+		<h4>Post Author: {post_data.author}</h4>
+		<hr/>
 		<p>{post_data.content}</p>
 	</div>)
 }

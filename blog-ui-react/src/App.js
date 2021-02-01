e6ae05e3bbd53b9f51d3d1c2a4adc5120dd4419f
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-01 08:48:50 +0200

    Added all routes to single switch

--- blog-ui-react/src/App.js
+++ blog-ui-react/src/App.js
@@ -4,23 +4,18 @@ import Title from './components/Title'
 import PostListing from './components/PostListing'
 import Post from './components/Post.js';
 import Navigation from './components/Navigation'
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <div>
             <Router>
                 <Navigation />
                 <Title />
                 <Switch>
-                    <Route path="/post" component={Post}></Route>
-                </Switch>
-                <Switch>
-                <Route path="/posts" component={PostListing}></Route>
+                    <Route path="/posts" exact component={PostListing}></Route>
+                    <Route path="/posts/:id" exact component={Post}></Route>
                 </Switch>
             </Router>
-        </div>
     )
 }
 

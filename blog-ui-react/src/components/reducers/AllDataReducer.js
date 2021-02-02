38b12c5b9e328c57e8d50b9f509576b2e3170fd3
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-03 00:51:29 +0200

    handled returning posts from reducer

--- blog-ui-react/src/components/reducers/AllDataReducer.js
+++ blog-ui-react/src/components/reducers/AllDataReducer.js
@@ -1,8 +1,9 @@
-import React, { setState } from 'react';
-
 const AllDataReducer = (state = [], action) => {
     switch(action.type){
         case 'GET_ALL_DATA':
+            fetch(action.payload)
+            .then(response => response.json())
+            .then(data => data.map(x => state.push(x)))
             return state
         default:
             return state;

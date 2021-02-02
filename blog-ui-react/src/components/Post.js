ac2cd5a52635fdee61a532b831d0c7c14a85ad4f
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-02 23:55:06 +0200

    Added separate containers for Post page

--- blog-ui-react/src/components/Post.js
+++ blog-ui-react/src/components/Post.js
@@ -6,10 +6,13 @@ const Post = ({ match }) => {
 	const post_data = usePostsData(`http://localhost:4449/api/posts/${match.params.id}`);
 	return (
 	<div className="PostContainer">
-		<h3>Post Title: {post_data.title}</h3>
-		<h4>Post Author: {post_data.author}</h4>
-		<hr/>
-		<p>{post_data.content}</p>
+		<div className="PostHeaderContainer">
+			<h3>Post Title: {post_data.title}</h3>
+			<h4>Post Author: {post_data.author}</h4>
+		</div>
+		<div className="PostContentContainer">
+			<p>{post_data.content}</p>
+		</div>
 	</div>)
 }
 

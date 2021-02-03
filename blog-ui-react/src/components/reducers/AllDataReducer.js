dc90f53603f710afacdc7ebaa3b65ecdf4acbbc2
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-03 11:46:16 +0200

    Bugfix: render all data from reducer with no refresh

--- blog-ui-react/src/components/reducers/AllDataReducer.js
+++ blog-ui-react/src/components/reducers/AllDataReducer.js
@@ -1,10 +1,13 @@
-const AllDataReducer = (state = [], action) => {
+const initialState = { posts: [] }
+
+const AllDataReducer = (state = initialState, action) => {
     switch(action.type){
         case 'GET_ALL_DATA':
+            let all_posts = []
             fetch(action.payload)
             .then(response => response.json())
-            .then(data => data.map(x => state.push(x)))
-            return state
+            .then(data => data.map(x => all_posts.push(x)))
+            return {...state, posts: all_posts}
         default:
             return state;
     }

fda47be82bbe4b4f620a2700c3eb1791a3788e37
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-03 11:46:48 +0200

    Simplified state return implementation (TODO)

--- blog-ui-react/src/components/reducers/PostDataReducer.js
+++ blog-ui-react/src/components/reducers/PostDataReducer.js
@@ -1,11 +1,13 @@
-const PostDataReducer = (state = {}, action) => {
+const initialState = { post: {} }
+
+const PostDataReducer = (state = initialState, action) => {
     switch(action.type){
         case 'GET_POST_DATA':
+            let post_data = {}
             fetch(action.payload)
             .then(response => response.json())
-            .then(data => Object.assign(state, data))
-            console.log(state)
-            return state
+            .then(data => Object.assign(post_data, data))
+            return {...state, post: post_data}
         default:
             return state;
     }

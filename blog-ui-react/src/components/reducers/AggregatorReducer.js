ffe7598b21e1cb4cd1281f0a5a863362bde52f8f
Cipri Rusu <cipri91rusu@gmail.com>
2021-02-03 10:03:46 +0200

    Added reducer in aggregator

--- blog-ui-react/src/components/reducers/AggregatorReducer.js
+++ blog-ui-react/src/components/reducers/AggregatorReducer.js
@@ -1,8 +1,10 @@
 import AllDataReducer from './AllDataReducer';
+import PostDataReducer from './PostDataReducer';
 import { combineReducers } from 'redux';
 
 const AggregatorReducer = combineReducers({
-    all_data: AllDataReducer
+    all_data: AllDataReducer,
+    post_data: PostDataReducer
 })
 
 export default AggregatorReducer;
